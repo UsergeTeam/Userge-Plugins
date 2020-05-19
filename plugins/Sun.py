2b4ad8a536f8d580fa4cad27bd39d67b9ebcf7c4
Shivam Vijaywargi <vijaywargishivam@gmail.com>
2020-05-19 17:07:11 +0530

    Added webupload, thunder, sun, quotes & usage plugins

--- plugins/Sun.py
+++ plugins/Sun.py
@@ -1,22 +1,25 @@
-import os
-import time
-from re import sub
+# Copyright (C) 2020 by UsergeTeam@Github, < https://github.com/UsergeTeam >.
+#
+# This file is part of < https://github.com/UsergeTeam/Userge > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/uaudith/Userge/blob/master/LICENSE >
+#
+# All rights reserved.
+
+
 from asyncio import sleep
 from collections import deque
-from random import choice, getrandbits, randint 
-import wget
-import requests
-from cowpy import cow 
 from userge import userge, Message
 
+
 @userge.on_cmd("sun$", about="__kensar sun animation__")
-async def sun_(message: Message): 
-    """sun""" 
+async def sun_(message: Message):
+    """sun"""
     deq = deque(list("😎🌤🌥☀️⛅️🌦🌞"))
-    try: 
-        for _ in range(32): 
-            await sleep(0.1) 
+    try:
+        for _ in range(32):
+            await sleep(0.1)
             await message.edit("".join(deq))
-            deq.rotate(1) 
-    except Exception: 
+            deq.rotate(1)
+    except Exception:
         await message.delete()
--- plugins/Thunder.py
+++ plugins/Thunder.py
@@ -1,22 +1,25 @@
-import os
-import time
-from re import sub
+# Copyright (C) 2020 by UsergeTeam@Github, < https://github.com/UsergeTeam >.
+#
+# This file is part of < https://github.com/UsergeTeam/Userge > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/uaudith/Userge/blob/master/LICENSE >
+#
+# All rights reserved.
+
+
 from asyncio import sleep
 from collections import deque
-from random import choice, getrandbits, randint 
-import wget
-import requests
-from cowpy import cow 
 from userge import userge, Message
 
+
 @userge.on_cmd("thunder$", about="__kensar thunder animation__")
-async def thunder_(message: Message): 
-    """thunder""" 
+async def thunder_(message: Message):
+    """thunder"""
     deq = deque(list("☀️🌤️⛅🌥️☁️🌩️🌧️⛈️⚡🌩️🌧️🌦️🌥️⛅🌤️☀️"))
-    try: 
-        for _ in range(32): 
-            await sleep(0.1) 
+    try:
+        for _ in range(32):
+            await sleep(0.1)
             await message.edit("".join(deq))
-            deq.rotate(1) 
-    except Exception: 
+            deq.rotate(1)
+    except Exception:
         await message.delete()
