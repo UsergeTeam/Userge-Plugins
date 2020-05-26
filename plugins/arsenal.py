19b8898f8047cd895e7594eb445b4df7e2c180ff
Phyco-Ninja <60768764+Phyco-Ninja@users.noreply.github.com>
2020-05-26 15:13:28 +0530

    Ok Boomer

--- plugins/arsenal.py
+++ plugins/arsenal.py
@@ -1,5 +1,5 @@
 import time
-import asyncio 
+import asyncio
 from userge import userge, Message
 
 @userge.on_cmd("benall",about={
@@ -12,7 +12,7 @@ async def ban_all(message: Message):
     await message.edit("Hold on Trying to ban all Members")
     async for to_ban in userge.iter_chat_members(chat):
         user = to_ban.user.id
-        await userge.kick_chat_member(chat, user) 
+        await userge.kick_chat_member(chat, user)
         ben_c += 1
         await asyncio.sleep(0.5)
     await message.edit(f"Banned {ben_c} members in {message.chat.title}")
@@ -27,7 +27,7 @@ async def keck_all(message: Message):
     await message.edit("Hold on Trying to kick all Members")
     async for to_kick in userge.iter_chat_members(chat):
         user = to_kick.user.id
-        await userge.kick_chat_member(chat, user, int(time.time()+60)) 
+        await userge.kick_chat_member(chat, user, int(time.time()+60))
         keck_c += 1
         await asyncio.sleep(0.5)
     await message.edit(f"Kicked {keck_c} members in {message.chat.title}")
