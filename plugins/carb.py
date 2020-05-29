567c215733fb89c82d07045408c3003269aec704
Sumanjay <cyberboysumanjay@gmail.com>
2020-05-29 16:08:05 +0530

    Pep8 Fix
    If you still have issues 
    GO F**K YOURSELF!

--- plugins/carb.py
+++ plugins/carb.py
@@ -10,17 +10,17 @@ import requests
     'header': "Create a carbon",
     'usage': "{tr}carb [text or reply to msg | theme-name | colour code]",
     'examples': "{tr}carb Carbon Plugin by Sumanjay | one-dark | #FF0000",
-    'themes':"3024-night, a11y-dark, blackboard, base16-dark, base16-light"
-        "cobalt, dracula, duotone-dark, hopscotch, lucario, material"
-        "monokai, night-owl, nord, oceanic-next, one-light, one-dark"
-        "panda-syntax, paraiso-dark, seti, shades-of-purple, solarized-dark"
-        "solarized-light, synthwave-84, twilight, verminal, vscode"
-        "yeti, zenburn"})
+    'themes': "3024-night, a11y-dark, blackboard, base16-dark, base16-light"
+    "cobalt, dracula, duotone-dark, hopscotch, lucario, material"
+    "monokai, night-owl, nord, oceanic-next, one-light, one-dark"
+    "panda-syntax, paraiso-dark, seti, shades-of-purple, solarized-dark"
+    "solarized-light, synthwave-84, twilight, verminal, vscode"
+    "yeti, zenburn"})
 async def carb(message: Message):
     # Setting Default Theme and Background Colour
     theme = 'dracula'
     bg = "rgba(144, 19, 254, 100)"
-    themes =  [
+    themes = [
         '3024-night', 'a11y-dark', 'blackboard', 'base16-dark', 'base16-light',
         'cobalt', 'dracula', 'duotone-dark', 'hopscotch', 'lucario', 'material',
         'monokai', 'night-owl', 'nord', 'oceanic-next', 'one-light', 'one-dark',
@@ -37,11 +37,11 @@ async def carb(message: Message):
         args = text.split('|')
 
     for arg in args:
-        arg  = arg.strip()
-        if arg.lower().replace(" ","-") in themes:
+        arg = arg.strip()
+        if arg.lower().replace(" ", "-") in themes:
             theme = arg.lower()
         elif arg.startswith("#") or arg.startswith("rgb"):
-            if arg[0]=="#":
+            if arg[0] == "#":
                 arg = arg[1:]
             bg = arg
         else:
@@ -51,9 +51,10 @@ async def carb(message: Message):
         await message.err("Input not found!")
         return
     await message.edit("⚡️ Carbonizing ⚡️")
-       
+
     try:
-        carbon_result = requests.get("https://sjprojectsapi.herokuapp.com/carbon/?text="+text+"&theme="+theme+"&bg="+bg).json()
+        carbon_result = requests.get(
+            "https://sjprojectsapi.herokuapp.com/carbon/?text="+text+"&theme="+theme+"&bg="+bg).json()
         await userge.send_photo(chat_id=message.chat.id, photo=carbon_result['link'])
         await message.delete()
     except Exception:
