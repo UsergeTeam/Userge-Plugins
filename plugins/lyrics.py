59c1363147feb4f3f7ce9c448ece44359339af85
Phyco-Ninja <60768764+Phyco-Ninja@users.noreply.github.com>
2020-05-30 14:34:08 +0530

    Updated with ne module

--- plugins/lyrics.py
+++ plugins/lyrics.py
@@ -1,31 +1,54 @@
-import asyncio
+import os
 from userge import userge, Message
-from PyLyrics import *
+from lyrics_extractor import Song_Lyrics
 
+GCS_ENGINE_ID = os.environ.get("GCS_ENGINE_ID", None) 
+GCS_API_KEY = os.environ.get("GCS_API_KEY", None)
 
-@userge.on_cmd("lyrics", about={
-    'header': "Get Lyrics of a Song",
-    'usage': "{tr}lyrics Singer Name - Song Name",
-    'examples': "{tr}lyrics Alan Walker - Faded"})
-async def lyrics(message: Message):
-    i = 0
+@userge.on_cmd("glyrics", about={
+    'header': "GCS Lyrics",
+    'description': "Scrape Song Lyrics from various websites"
+                   " using your Custom Search Engine.",
+    'usage': "{tr}glyrics [Song Name]",
+    'examples': "`{tr}glyrics Swalla Nicki Minaj`"})
+async def glyrics(message: Message):
+    if (GCS_ENGINE_ID and GCS_API_KEY) is None:
+        await message.edit(
+            "**Requirements Missing**\n\n"
+            "Please Configure `GCS_ENGINE_ID` & `GCS_API_KEY`\n\n"
+            "**More Info on How to Configure:**\n"
+            "[NOTE: Read all Steps First, Coz No one's gonna help ya]\n"
+            "1. Create your new Custom Search Engine here to get your Engine ID: "
+            "https://cse.google.com/cse/create/new"
+            "2. Add any of the following or all (adding all is recommended) "
+            "websites as per your choice in your Custom Search Engine:\n"
+            " » `https://genius.com/` \n"
+            " » `http://www.lyricsted.com/` \n"
+            " » `http://www.lyricsbell.com/` \n"
+            " » `https://www.glamsham.com/` \n"
+            " » `http://www.lyricsoff.com/` \n"
+            " » `http://www.lyricsmint.com/` \n"
+            "**NOTE:** Please **don't turn on** the '**Search the entire Web**'"
+            " feature as it is currently not possible to scrape from any random"
+            " sites appearing in the search results.\n"
+            "3. Visit here to get your API key: https://developers.google.com/custom-search/v1/overview"
+            "\n4. Finally Add following in Heroku Config vars:\n"
+            "**Key:** `GCS_ENGINE_ID` \n**Value:** Engine ID of CSE (got in Step 1)\n"
+            "**Key:** `GCS_API_KEY`\n**Value:** API Token (got in Step 3)")
+        return
 
-    args = message.input_str
-    if not args:
-        await message.edit("Bruh, Give some input.\nDo .help lyrics plox")
+    if not message.input_str:
+        await message.edit("Give Song Name Peru Uzer")
+        return
+
+    song = message.input_str
     try:
-        song = args.split("-")
-        if len(song) == 1:
-            await message.edit("Do .help lyrics")
+        gcsl = Song_Lyrics(GCS_API_KEY, GCS_ENGINE_ID)
+        song_title, song_lyrics = gcsl.get_lyrics(song)
+        if song_lyrics:
+            out = f"**Here is {song_title}**\n\n\n__{song_lyrics}__"
         else:
-            await message.edit("ðï¸Searching lyrics")
-            lyrics = PyLyrics.getLyrics(song[0].strip(), song[1].strip()).split("\n")
-            lyric_message = f"Lyrics {song[0].strip()} from {song[1].strip()} ð"
-            lyric_message += "\n\n" + "\n".join(lyrics)
-            try:
-                await message.edit_or_send_as_file(lyric_message)
-            except Exception as e:
-                 LOG.exception('Received exception during gbannedList')
-                 await message.edit("Error: "+str(e))
-    except ValueError:
-        await message.edit("Song not found")
+            out = f"**{song_title}**\n ┐(´д｀)┌"
+        await message.edit(out)
+    except Exception as e:
+        await message.err(e)
