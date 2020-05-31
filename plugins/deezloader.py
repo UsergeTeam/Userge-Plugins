8bcfc23b786b72e958dc62730e9285969b402905
Phyco-Ninja <60768764+Phyco-Ninja@users.noreply.github.com>
2020-05-31 22:54:07 +0530

    More User Friendly? 🌚🤧

--- plugins/deezloader.py
+++ plugins/deezloader.py
@@ -5,28 +5,28 @@ from userge import userge, Message
 
 ARL_TOKEN = os.environ.get("ARL_TOKEN", None)
 PATH = 'deezdown_temp/'
+ARL_HELP = """**Oops, Time to Help Yourself**
+[Here Help Yourself](https://www.google.com/search?q=how+to+get+deezer+arl+token)
+
+After getting Arl token Config `ARL_TOKEN` var in heroku"""
 
 
 @userge.on_cmd("deezload", about={
-    'header': "Name's Deez, DeezLoader",
+    'header': "DeezLoader for Userge",
     'description': "Download Songs/Albums/Playlists via "
                    "Sopitfy or Deezer Links. "
                    "\n**NOTE:** Music Quality is optional",
-    'flags': {'-stl': "Download a Track via Spotify Link",
-              '-dtl': "Download a Track via Deezer Link",
-              '-sal': "Download an Album via Spotify Link",
-              '-dal': "Download an Album via Deezer Link",
-              '-spl': "Download a Playlist via Spotify Link",
-              '-dpl': "Download a Playlist via Deezer Link",
+    'flags': {'-sdl': "Download via Spotify Link",
+              '-ddl': "Download via Deezers Link",
               '-dsong': "Download a Song by passing Artist Name and Song Name",
               '-zip': "Get a zip archive for Albums/Playlist Download"},
     'options': "Available Sound Quality: `FLAC` | `MP3_320` | `MP3_256` | `MP3_128`",
     'usage': "{tr}deezload [flag] [link | quality (default MP3_320)]",
-    'examples': "{tr}deezload -dtl https://www.deezer.com/track/142750222 \n"
-                "{tr}deezload -dtl https://www.deezer.com/track/3824710 FLAC \n"
-                "{tr}deezload -dal https://www.deezer.com/album/1240787 FLAC \n"
-                "{tr}deezload -dal -zip https://www.deezer.com/album/1240787 \n"
-                "{tr}deezload -dsong Ed Sheeran - Shape of You - MP3_256 (quality is optional)"})
+    'examples': "{tr}deezload -ddl https://www.deezer.com/track/142750222 \n"
+                "{tr}deezload -ddl https://www.deezer.com/track/3824710 FLAC \n"
+                "{tr}deezload -ddl https://www.deezer.com/album/1240787 FLAC \n"
+                "{tr}deezload -ddl -zip https://www.deezer.com/album/1240787 \n"
+                "{tr}deezload -dsong Ed Sheeran - Shape of You"})
 async def deezload(message: Message):
     if not os.path.exists(PATH):
         os.makedirs(PATH)
@@ -36,11 +36,7 @@ async def deezload(message: Message):
         return
     await message.edit("Trying to Login 🥴")
     if ARL_TOKEN is None:
-        await message.edit(
-            "Oops, Time to Help Yourself "
-            "\n[Here Help Yourself](https://www.google.com/search?q=how+to+get+deezer+arl+token)"
-            "\nAfter getting Arl token Config `ARL_TOKEN` var in heroku"
-        )
+        await message.edit(ARL_HELP, disable_web_page_preview=True)
         return
     try:
         loader = deezloader.Login(ARL_TOKEN)
@@ -82,169 +78,16 @@ async def deezload(message: Message):
                 await message.edit("WeW, Use that thing which is present on top floor of ur body 🌚")
                 return
 
-    if '-stl' in flags:
-        await message.edit("Trying to download song via Spotify Link 🥴")
-        track = loader.download_trackspo(
-            input_link,
-            output=PATH,
-            quality=quality,
-            recursive_quality=True,
-            recursive_download=True,
-            not_interface=True
-        )
-        await message.edit("Now Uploading 📤")
-        await userge.send_audio(
-            chat_id=message.chat.id,
-            audio=track
-        )
-    elif '-dtl' in flags:
-        await message.edit("trying to download song via Deezer Link 🥴")
-        track = loader.download_trackdee(
-            input_link,
-            output=PATH,
-            quality=quality,
-            recursive_quality=True,
-            recursive_download=True,
-            not_interface=True
-        )
-        await message.edit("Now Uploading 📤")
-        await userge.send_audio(
-            chat_id=message.chat.id,
-            audio=track
-        )
-
-    if '-sal' in flags:
-        await message.edit("Trying to download album 🤧")
-        if to_zip:
-            _, zip_ = loader.download_albumspo(
-                input_link,
-                output=PATH,
-                quality=quality,
-                recursive_quality=True,
-                recursive_download=True,
-                not_interface=True,
-                zips=to_zip
-            )
-            await message.edit("Sending as Zip File 🗜")
-            await userge.send_document(
-                chat_id=message.chat.id,
-                document=zip_
-            )
-        else:
-            album_list = loader.download_albumspo(
-                input_link,
-                output=PATH,
-                quality=quality,
-                recursive_quality=True,
-                recursive_download=True,
-                not_interface=True,
-                zips=to_zip)
-            await message.edit("Uploading Tracks 📤")
-            for tracks in album_list:
-                await userge.send_audio(
-                    chat_id=message.chat.id,
-                    audio=tracks
-                )
-    elif '-dal' in flags:
-        await message.edit("Trying to download album 🤧")
-        if to_zip:
-            _, zip_ = loader.download_albumdee(
-                input_link,
-                output=PATH,
-                quality=quality,
-                recursive_quality=True,
-                recursive_download=True,
-                not_interface=True,
-                zips=to_zip
-            )
-            await message.edit("Uploading as Zip File 🗜")
-            await userge.send_document(
-                chat_id=message.chat.id,
-                document=zip_
-            )
-        else:
-            album_list = loader.download_albumdee(
-                input_link,
-                output=PATH,
-                quality=quality,
-                recursive_quality=True,
-                recursive_download=True,
-                not_interface=True,
-                zips=to_zip
-            )
-            await message.edit("Uploading Tracks 📤")
-            for tracks in album_list:
-                await userge.send_audio(
-                    chat_id=message.chat.id,
-                    audio=tracks
-                )
-
-    if '-spl' in flags:
-        await message.edit("Trying to download Playlist 🎶")
-        if to_zip:
-            _, zip_ = loader.download_playlistspo(
-                input_link,
-                output=PATH,
-                quality=quality,
-                recursive_quality=True,
-                recursive_download=True,
-                not_interface=True,
-                zips=to_zip
-            )
-            await message.edit("Sending as Zip 🗜")
-            await userge.send_document(
-                chat_id=message.chat.id,
-                document=zip_
-            )
-        else:
-            album_list = loader.download_playlistspo(
-                input_link,
-                output=PATH,
-                quality=quality,
-                recursive_quality=True,
-                recursive_download=True,
-                not_interface=True,
-                zips=to_zip
-            )
-            await message.edit("Uploading Tracks 📤")
-            for tracks in album_list:
-                await userge.send_audio(
-                    chat_id=message.chat.id,
-                    audio=tracks
-                )
-    elif '-dpl' in flags:
-        await message.edit("Trying to download Playlist 🎶")
-        if to_zip:
-            _, zip_ = loader.download_playlistdee(
-                input_link,
-                output=PATH,
-                quality=quality,
-                recursive_quality=True,
-                recursive_download=True,
-                not_interface=True,
-                zips=to_zip
-            )
-            await message.edit("Sending as Zip File 🗜")
-            await userge.send_document(
-                chat_id=message.chat.id,
-                document=zip_
-            )
-        else:
-            album_list = loader.download_playlistdee(
-                input_link,
-                output=PATH,
-                quality=quality,
-                recursive_quality=True,
-                recursive_download=True,
-                not_interface=True,
-                zips=to_zip
-            )
-            await message.edit("Uploading Tracks 📤")
-            for tracks in album_list:
-                await userge.send_audio(
-                    chat_id=message.chat.id,
-                    audio=tracks
-                )
+    if '-sdl' in flags:
+        if 'track/' in input_link:
+            await proper_trackdl(input_link, quality, message, loader, PATH, userge)
+        elif 'album/' or 'playlist/' in input_link:
+            await batch_dl(input_link, quality, message, loader, PATH, userge, to_zip)
+    elif '-ddl' in flags:
+        if 'track/' in input_link:
+            await proper_trackdl(input_link, quality, message, loader, PATH, userge)
+        elif 'album/' or 'playlist' in input_link:
+            await batch_dl(input_link, quality, message, loader, PATH, userge, to_zip)
 
     if '-dsong' in flags:
         await message.edit("Searching for Song 🔍")
@@ -267,3 +110,170 @@ async def deezload(message: Message):
             await message.edit("Song not Found 🚫")
     await message.delete()
     shutil.rmtree(PATH, ignore_errors=True)
+
+async def proper_trackdl(link, qual, msg, client, dir, u):
+    if 'spotify' in link:
+        await msg.edit("Trying to download song via Spotify Link 🥴")
+        track = client.download_trackspo(
+            link,
+            output=dir,
+            quality=qual,
+            recursive_quality=True,
+            recursive_download=True,
+            not_interface=True
+        )
+        await msg.edit("Now Uploading 📤")
+        await u.send_audio(
+            chat_id=msg.chat.id,
+            audio=track
+        )
+    elif 'deezer' in link:
+        await msg.edit("Trying to download song via Deezer Link 🥴")
+        track = client.download_trackdee(
+            link,
+            output=dir,
+            quality=qual,
+            recursive_quality=True,
+            recursive_download=True,
+            not_interface=True
+        )
+        await mag.edit("Now Uploading 📤")
+        await u.send_audio(
+            chat_id=msg.chat.id,
+            audio=track
+        )
+async def batch_dl(link, qual, msg, client, dir, u, allow_zip):
+    if 'spotify' in link:
+        if 'album/' in link:
+            await msg.edit("Trying to download album 🤧")
+            if allow_zip:
+                _, zip_ = client.download_albumspo(
+                    link,
+                    output=dir,
+                    quality=qual,
+                    recursive_quality=True,
+                    recursive_download=True,
+                    not_interface=True,
+                    zips=True
+                )
+                await msg.edit("Sending as Zip File 🗜")
+                await u.send_document(
+                    chat_id=msg.chat.id,
+                    document=zip_
+                )
+            else:
+                album_list = client.download_albumspo(
+                    link,
+                    output=dir,
+                    quality=qual
+                    recursive_quality=True,
+                    recursive_download=True,
+                    not_interface=True,
+                    zips=False)
+                await msg.edit("Uploading Tracks 📤")
+                for tracks in album_list:
+                    await u.send_audio(
+                        chat_id=msg.chat.id,
+                        audio=tracks
+                    )
+        if 'playlist/' in link:
+            await msg.edit("Trying to download Playlist 🎶")
+            if allow_zip:
+                _, zip_ = client.download_playlistspo(
+                    link,
+                    output=dir,
+                    quality=qual,
+                    recursive_quality=True,
+                    recursive_download=True,
+                    not_interface=True,
+                    zips=True
+                )
+                await msg.edit("Sending as Zip 🗜")
+                await u.send_document(
+                    chat_id=msg.chat.id,
+                    document=zip_
+                )
+            else:
+                album_list = client.download_playlistspo(
+                    link,
+                    output=dir,
+                    quality=qual,
+                    recursive_quality=True,
+                    recursive_download=True,
+                    not_interface=True,
+                    zips=False
+                )
+                await msg.edit("Uploading Tracks 📤")
+                for tracks in album_list:
+                    await u.send_audio(
+                        chat_id=msg.chat.id,
+                        audio=tracks
+                    )
+
+    if 'deezer' in link:
+        if 'album/' in link:
+            await msg.edit("Trying to download album 🤧")
+            if allow_zip:
+                _, zip_ = client.download_albumdee(
+                    link,
+                    output=dir,
+                    quality=qual,
+                    recursive_quality=True,
+                    recursive_download=True,
+                    not_interface=True,
+                    zips=True
+                )
+                await msg.edit("Uploading as Zip File 🗜")
+                await u.send_document(
+                    chat_id=msg.chat.id,
+                    document=zip_
+                )
+            else:
+                album_list = client.download_albumdee(
+                    link,
+                    output=dir,
+                    quality=qual,
+                    recursive_quality=True,
+                    recursive_download=True,
+                    not_interface=True,
+                    zips=False
+                )
+                await msg.edit("Uploading Tracks 📤")
+                for tracks in album_list:
+                    await u.send_audio(
+                        chat_id=msgchat.id,
+                        audio=tracks
+                    )
+        elif 'playlist/' in link:
+            await message.edit("Trying to download Playlist 🎶")
+            if allow_zip:
+                _, zip_ = client.download_playlistdee(
+                    link,
+                    output=dir,
+                    quality=qual,
+                    recursive_quality=True,
+                    recursive_download=True,
+                    not_interface=True,
+                    zips=True
+                )
+                await msg.edit("Sending as Zip File 🗜")
+                await u.send_document(
+                    chat_id=msg.chat.id,
+                    document=zip_
+                )
+            else:
+                album_list = client.download_playlistdee(
+                    link,
+                    output=dir,
+                    quality=qual,
+                    recursive_quality=True,
+                    recursive_download=True,
+                    not_interface=True,
+                    zips=False
+                )
+                await msg.edit("Uploading Tracks 📤")
+                for tracks in album_list:
+                    await u.send_audio(
+                        chat_id=msg.chat.id,
+                        audio=tracks
+                    )
