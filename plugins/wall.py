6a1d5b83faf8ef1c79f21291086f8f8a6ed72355
NobArxtx <54905770+NobArxtx@users.noreply.github.com>
2020-06-12 16:10:30 +0530

    🙄

--- plugins/wall.py
+++ plugins/wall.py
@@ -2,7 +2,7 @@ from userge import userge, Message, Config
 from bs4 import BeautifulSoup as soup
 import requests
 from random import randint, choice
-import os
+import os, subprocess
 
 
 async def dlimg(link):
@@ -11,7 +11,6 @@ async def dlimg(link):
     path_i = os.path.join(Config.DOWN_PATH, paea)
     with open(path_i, 'wb') as k:
         k.write(e)
-        k.close()
     return path_i
 
 
@@ -45,37 +44,36 @@ async def wall(strin: str):
         except Exception:
             page_num = 1
         n = randint(1, page_num)
-        if True:
-            if page_num != 1:
-                if check_link:
-                    resp = requests.get(page_link.format(n))
-                else:
-                    resp = requests.get(page_link.format(strin, n))
-                resp = soup(resp.content, 'lxml')
-            a_s = resp.find_all('a')
-            list_a_s = []
-            tit_links = []
-            r = ['thumb', '350', 'img', 'big.php?i', 'data-src', 'title']
-            for a_tag in a_s:
-                if all(d in str(a_tag) for d in r):
-                    list_a_s.append(a_tag)
-            try:
-                for df in list_a_s:
-                    imgi = df.find('img')
-                    li = str(imgi['data-src']).replace('thumb-350-', '')
-                    titl = str(df['title']).replace('|', '')
-                    titl = titl.replace('  ', '')
-                    titl = titl.replace('Image', '')
-                    titl = titl.replace('HD', '')
-                    titl = titl.replace('Wallpaper', '')
-                    titl = titl.replace('Background', '')
-                    p = (li, titl)
-                    tit_links.append(p)
-            except Exception:
-                pass
-            del list_a_s
-            tit_link = choice(tit_links)
-            return tit_link
+        if page_num != 1:
+            if check_link:
+                resp = requests.get(page_link.format(n))
+            else:
+                resp = requests.get(page_link.format(strin, n))
+            resp = soup(resp.content, 'lxml')
+        a_s = resp.find_all('a')
+        list_a_s = []
+        tit_links = []
+        r = ['thumb', '350', 'img', 'big.php?i', 'data-src', 'title']
+        for a_tag in a_s:
+            if all(d in str(a_tag) for d in r):
+                list_a_s.append(a_tag)
+        try:
+            for df in list_a_s:
+                imgi = df.find('img')
+                li = str(imgi['data-src']).replace('thumb-350-', '')
+                titl = str(df['title']).replace('|', '')
+                titl = titl.replace('  ', '')
+                titl = titl.replace('Image', '')
+                titl = titl.replace('HD', '')
+                titl = titl.replace('Wallpaper', '')
+                titl = titl.replace('Background', '')
+                p = (li, titl)
+                tit_links.append(p)
+        except Exception:
+            pass
+        del list_a_s
+        tit_link = choice(tit_links)
+        return tit_link
 
 
 @userge.on_cmd("wall", about={
@@ -115,7 +113,8 @@ Search Query : {}'''.format(message.input_str)
                 await message.edit(e)
                 return
             await message.delete()
-            os.system('rm {}'.format(idl))
+            
+            subprocess.call(['rm',idl])
         else:
             await message.edit('**Result Not Found**')
             await message.reply_sticker('CAADBAADIQEAAl_GARknbPJaYsVA2xYE')
