f46f5f029b8da089651c0d086571b43b6d943097
Phyco-Ninja <60768764+Phyco-Ninja@users.noreply.github.com>
2020-06-13 21:35:08 +0900

    Updated Lyrics  (#55)
    * Missing Requirement
    
    * Update tts.py
    
    * To be continued
    
    * Um good
    
    * Update lyrics.py
    
    * 🤔
    
    * some fixes
    
    * remove space
    
    Co-authored-by: rking32 <sljohnwick@gmail.com>
    Co-authored-by: rking32 <52490534+rking32@users.noreply.github.com>

--- plugins/lyrics.py
+++ plugins/lyrics.py
@@ -1,58 +1,48 @@
 import os
+import re
 
-from lyrics_extractor import Song_Lyrics
+import requests
+from bs4 import BeautifulSoup
+from googlesearch import search
 
 from userge import userge, Message
 
-GCS_ENGINE_ID = os.environ.get("GCS_ENGINE_ID", None)
-GCS_API_KEY = os.environ.get("GCS_API_KEY", None)
-
 
 @userge.on_cmd("glyrics", about={
-    'header': "GCS Lyrics",
-    'description': "Scrape Song Lyrics from various websites"
-                   " using your Custom Search Engine.",
+    'header': "Genius Lyrics",
+    'description': "Scrape Song Lyrics from Genius.com",
     'usage': "{tr}glyrics [Song Name]",
     'examples': "{tr}glyrics Swalla Nicki Minaj"})
 async def glyrics(message: Message):
-    if (GCS_ENGINE_ID and GCS_API_KEY) is None:
-        await message.edit(
-            "**Requirements Missing**\n\n"
-            "Please Configure `GCS_ENGINE_ID` & `GCS_API_KEY`\n\n"
-            "**More Info on How to Configure:**\n"
-            "[NOTE: Read all Steps First, Coz No one's gonna help ya]\n"
-            "1. Create your new Custom Search Engine here to get your Engine ID: "
-            "https://cse.google.com/cse/create/new"
-            "2. Add any of the following or all (adding all is recommended) "
-            "websites as per your choice in your Custom Search Engine:\n"
-            " » `https://genius.com/` \n"
-            " » `http://www.lyricsted.com/` \n"
-            " » `http://www.lyricsbell.com/` \n"
-            " » `https://www.glamsham.com/` \n"
-            " » `http://www.lyricsoff.com/` \n"
-            " » `http://www.lyricsmint.com/` \n"
-            "**NOTE:** Please **don't turn on** the '**Search the entire Web**'"
-            " feature as it is currently not possible to scrape from any random"
-            " sites appearing in the search results.\n"
-            "3. Visit here to get your API key: "
-            "https://developers.google.com/custom-search/v1/overview"
-            "\n4. Finally Add following in Heroku Config vars:\n"
-            "**Key:** `GCS_ENGINE_ID` \n**Value:** Engine ID of CSE (got in Step 1)\n"
-            "**Key:** `GCS_API_KEY`\n**Value:** API Token (got in Step 3)")
-        return
-
-    if not message.input_str:
-        await message.edit("Give Song Name Peru Uzer")
-        return
-
     song = message.input_str
-    try:
-        gcsl = Song_Lyrics(GCS_API_KEY, GCS_ENGINE_ID)
-        song_title, song_lyrics = gcsl.get_lyrics(song)
-        if song_lyrics:
-            out = f"**Here is {song_title}**\n\n\n__{song_lyrics}__"
-        else:
-            out = f"**{song_title}**\n ┐(´д｀)┌"
-        await message.edit(out)
-    except Exception as e:
-        await message.err(e)
+    if not song:
+        await message.edit("Bruh WTF?")
+        return
+    await message.edit(f"__Searching Lyrics For {song}__")
+    to_search = song + "genius lyrics"
+    gen_surl = list(search(to_search, num=1, stop=1))[0]
+    gen_page = requests.get(gen_surl)
+    scp = BeautifulSoup(gen_page.text, 'html.parser')
+    lyrics = scp.find("div", class_="lyrics").get_text()
+    lyrics = re.sub(r'[\(\[].*?[\)\]]', '', lyrics)
+    lyrics = os.linesep.join([s for s in lyrics.splitlines() if s])
+    title = scp.find('title').get_text().split("|")
+    writers_box = [
+        writer
+        for writer in scp.find_all("span", {'class': 'metadata_unit-label'})
+        if writer.text == "Written By"]
+    if writers_box:
+        target_node = writers_box[0].find_next_sibling("span", {'class': 'metadata_unit-info'})
+        writers = target_node.text.strip()
+    else:
+        writers = "UNKNOWN"
+    lyr_format = ''
+    lyr_format += '**' + title[0] + '**\n'
+    lyr_format += '__' + lyrics + '__'
+    lyr_format += "\n\n**Written By: **" + '__' + writers + '__'
+    lyr_format += "\n**Source: **" + '`' + title[1] + '`'
+
+    if lyr_format:
+        await message.edit(lyr_format)
+    else:
+        await message.edit(f"No Lyrics Found for **{song}**")
