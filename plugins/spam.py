d16568e022707166b4e4b64807f09c3ce873430c
rking32 <sljohnwick@gmail.com>
2020-06-14 11:56:48 +0530

    delete old spam and spem to spam 🤔🙄😁

--- plugins/spam.py
+++ plugins/spam.py
@@ -1,16 +1,73 @@
-# by the unknown
-
+import os
 import asyncio
 
-from userge import userge, Message
+from userge import userge, Message, Config
+
+S_LOG = userge.getCLogger(__name__)
 
 
-@userge.on_cmd("spam$", about={'header': "THE SPAMMER"})
-async def spam_func(message: Message):
-    animation_chars = [
-        "YOU", "ARE", "A", "REAL", "SPAMMER", "THAT", "I", "HAVE", "SEEN",
-        "YOU ARE A REAL SPAMMER THAT I HAVE SEEN",
-    ]
-    for i in range(10):
-        await asyncio.sleep(0.3)
-        await message.edit(animation_chars[i])
+@userge.on_cmd("spam", about={
+    'header': "Spam some Messages",
+    'description': "Message Spam module just for fun."
+                   "Btw Don't over use this plugin or get"
+                   "ready for account ban or flood waits. "
+                   "For spamming text use '|' to separate count and text.",
+    'usage': "{tr}spam [spam count] | [spam message/reply to a media]",
+    'examples': "**For Text:** `{tr}spam 2 | Durov will ban me for using this plugin`"})
+async def spam(message: Message):
+    replied = message.reply_to_message
+    is_str = "|" in message.input_str
+    if (replied and replied.media and not is_str):
+        if not os.path.isdir(Config.DOWN_PATH):
+            os.makedirs(Config.DOWN_PATH)
+        if replied.sticker:
+            to_spam = replied.sticker.file_id
+            try:
+                sc = int(message.input_str)
+            except ValueError as e:
+                await message.edit(e)
+                await message.reply_sticker(sticker="CAADAQADzAADiO9hRu2b2xyV4IbAFgQ")
+                return
+            await message.edit(f"Spamming {sc} Time")
+            for _ in range(sc):
+                await userge.send_sticker(sticker=to_spam, chat_id=message.chat.id)
+                await asyncio.sleep(0.1)
+            await S_LOG.log(f"Spammed Sticker in Chat» {message.chat.title}, {sc} times")
+        elif (replied.animation or replied.video or replied.photo):
+            dls = await userge.download_media(
+                message=message.reply_to_message,
+                file_name=Config.DOWN_PATH
+            )
+            to_spam = os.path.join(Config.DOWN_PATH, os.path.basename(dls))
+            try:
+                sc = int(message.input_str)
+            except ValueError as e:
+                await message.edit(e)
+                await message.reply_sticker(sticker="CAADAQADzAADiO9hRu2b2xyV4IbAFgQ")
+                return
+            await message.edit(f"Spamming {sc} times")
+            if (replied.video or replied.animation):
+                for _ in range(sc):
+                    await userge.send_video(video=to_spam, chat_id=message.chat.id)
+                    await asyncio.sleep(0.1)
+            elif replied.photo:
+                for _ in range(sc):
+                    await userge.send_photo(photo=to_spam, chat_id=message.chat.id)
+                    await asyncio.sleep(0.1)
+            await S_LOG.log(f"Spammed Media in Chat» {message.chat.title}, {sc} times")
+    elif is_str:
+        spam_count, spam_text = message.input_str.split("|")
+        try:
+            sc = int(spam_count)
+        except ValueError as e:
+            await message.edit(e)
+            await message.reply_sticker(sticker="CAADAQADzAADiO9hRu2b2xyV4IbAFgQ")
+            return
+        await message.edit(f"Spamming {sc} times")
+        for _ in range(sc):
+            await userge.send_message(text=spam_text, chat_id=message.chat.id)
+            await asyncio.sleep(0.1)
+        await S_LOG.log(f"Spammed Text in Chat» {message.chat.title}, {sc} times")
+    else:
+        await message.edit("Well it doesn't work that way")
+        await message.reply_sticker(sticker="CAADAQAD6gADfAVQRnyVSb3GhGT4FgQ")
