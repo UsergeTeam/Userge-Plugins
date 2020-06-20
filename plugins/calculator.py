9cc0dd6290582db729f19a89e93f6a162e032c7d
Krishna-singhal <65902764+rowdy-hero@users.noreply.github.com>
2020-06-20 18:31:13 +0530

    😅

--- plugins/calculator.py
+++ plugins/calculator.py
@@ -4,7 +4,6 @@
 
 import math
 import asyncio
-from math import pi, e
 
 from userge import userge, Message
 
@@ -17,25 +16,26 @@ async def add_(message: Message):
 
     number = message.input_str
     if not number:
-        await message.err(f"```input not found!```")
+        await message.err("```input not found!```")
         return
 
     if "+" not in message.input_str:
-        await message.err(f"```both numbers required in the form of X + Y```")
+        await message.err("```both numbers required in the form of X + Y```")
         return
 
-    await message.edit(f"```processing...```")
+    await message.edit("```processing...```")
     await asyncio.sleep(1)
 
     number_1, number_2 = message.input_str.split('+', maxsplit=1)
     if not number_2:
-        await message.err(f"```Second number required for add```")
+        await message.err("```Second number required for add```")
         return
     try:
 
         result = (int(number_1.strip()) + int(number_2.strip()))
 
-        await message.edit(f"**OUTPUT:**\n`{int(result)}`")
+        await message.edit("<b>OUTPUT:</b>\n<code>{}</code>".format(result),
+        parse_mode='html')
     except Exception as e:
         await message.err(e)
 
@@ -48,25 +48,26 @@ async def subtract_(message: Message):
 
     number = message.input_str
     if not number:
-        await message.err(f"```input not found!```")
+        await message.err("```input not found!```")
         return
 
     if "-" not in message.input_str:
-        await message.err(f"```both numbers required in the form of X - Y```")
+        await message.err("```both numbers required in the form of X - Y```")
         return
 
-    await message.edit(f"```processing...```")
+    await message.edit("```processing...```")
     await asyncio.sleep(1)
 
     number_1, number_2 = message.input_str.split('-', maxsplit=1)
     if not number_2:
-        await message.err(f"```Second number required for subtract```")
+        await message.err("```Second number required for subtract```")
         return
     try:
 
         result = (int(number_1.strip()) - int(number_2.strip()))
 
-        await message.edit(f"**OUTPUT:**\n`{int(result)}`")
+        await message.edit("<b>OUTPUT:</b>\n<code>{}</code>".format(result),
+        parse_mode='html')
     except Exception as e:
         await message.err(e)
 
@@ -79,24 +80,25 @@ async def multiply_(message: Message):
 
     number = message.input_str
     if not number:
-        await message.err(f"```input not found!```")
+        await message.err("```input not found!```")
         return
 
     if "*" not in message.input_str:
-        await message.err(f"```both numbers required in the form of X * Y```")
+        await message.err("```both numbers required in the form of X * Y```")
         return
 
-    await message.edit(f"```processing...```")
+    await message.edit("```processing...```")
     await asyncio.sleep(1)
 
     number_1, number_2 = message.input_str.split('*', maxsplit=1)
     if not number_2:
-        await message.err(f"```Second number required for multiplication```")
+        await message.err("```Second number required for multiplication```")
         return
     try:
         result = (int(number_1.strip()) * int(number_2.strip()))
 
-        await message.edit(f"**OUTPUT:**\n`{int(result)}`")
+        await message.edit("<b>OUTPUT:</b>\n<code>{}</code>".format(result),
+        parse_mode='html')
     except Exception as e:
         await message.err(e)
 
@@ -109,24 +111,25 @@ async def divide_(message: Message):
 
     number = message.input_str
     if not number:
-        await message.err(f"```input not found!```")
+        await message.err("```input not found!```")
         return
 
     if "/" not in message.input_str:
-        await message.err(f"```both numbers required in the form of X / Y```")
+        await message.err("```both numbers required in the form of X / Y```")
         return
 
-    await message.edit(f"```processing...```")
+    await message.edit("```processing...```")
     await asyncio.sleep(1)
 
     number_1, number_2 = message.input_str.split('/', maxsplit=1)
     if not number_2:
-        await message.err(f"```Second number required for division```")
+        await message.err("```Second number required for division```")
         return
     try:
         result = (int(number_1.strip()) / int(number_2.strip()))
 
-        await message.edit(f"**OUTPUT:**\n`{int(result)}`")
+        await message.edit("<b>OUTPUT:</b>\n<code>{}</code>".format(result),
+        parse_mode='html')
     except Exception as e:
         await message.err(e)
 
@@ -139,54 +142,58 @@ async def fdivide_(message: Message):
 
     number = message.input_str
     if not number:
-        await message.err(f"```input not found!```")
+        await message.err("```input not found!```")
         return
 
     if "/" not in message.input_str:
-        await message.err(f"```both numbers required in the form of X / Y```")
+        await message.err("```both numbers required in the form of X / Y```")
         return
 
-    await message.edit(f"```processing...```")
+    await message.edit("```processing...```")
     await asyncio.sleep(1)
 
     number_1, number_2 = message.input_str.split('/', maxsplit=1)
     if not number_2:
-        await message.err(f"```Second number required to get quotient of two numbers```")
+        await message.err("```Second number required to get quotient of two numbers```")
         return
     try:
         result = (int(number_1.strip()) // int(number_2.strip()))
 
-        await message.edit(f"**OUTPUT:**\n`{int(result)}`")
+        await message.edit("<b>OUTPUT:</b>\n<code>{}</code>".format(result),
+        parse_mode='html')
     except Exception as e:
         await message.err(e)
 
 
 @userge.on_cmd("modulo", about={
-    'header': "Modulo operation finds the remainder or signed remainder after division of one number by another.",
+    'header': "Modulo operation finds the\n"
+    "remainder or signed remainder after\n"
+    "division of one number by another.",
     'usage': "{tr}modulo [ X % Y ]\n\n{tr}modulo 6969 % 6969"})
 async def modulo_(message: Message):
     """Use to get only remainder of two numbers"""
 
     number = message.input_str
     if not number:
-        await message.err(f"```input not found!```")
+        await message.err("```input not found!```")
         return
 
     if "%" not in message.input_str:
-        await message.err(f"```both numbers required in the form of X % Y```")
+        await message.err("```both numbers required in the form of X % Y```")
         return
 
-    await message.edit(f"```processing...```")
+    await message.edit("```processing...```")
     await asyncio.sleep(1)
 
     number_1, number_2 = message.input_str.split('%', maxsplit=1)
     if not number_2:
-        await message.err(f"```Second number required to get modulo of two numbers```")
+        await message.err("```Second number required to get modulo of two numbers```")
         return
     try:
         result = (int(number_1.strip()) % int(number_2.strip()))
 
-        await message.edit(f"**OUTPUT:**\n`{int(result)}`")
+        await message.edit("<b>OUTPUT:</b>\n<code>{}</code>".format(result),
+        parse_mode='html')
     except Exception as e:
         await message.err(e)
 
@@ -199,14 +206,15 @@ async def factorial_(message: Message):
 
     number = message.input_str
     if not number:
-        await message.err(f"```input not found!```")
+        await message.err("```input not found!```")
         return
 
-    await message.edit(f"```processing...```")
+    await message.edit("```processing...```")
     await asyncio.sleep(1)
 
     result = math.factorial(int(number))
-    await message.edit(f"**OUTPUT:**\n`{number}`! = `{result}`")
+    await message.edit("<b>OUTPUT:</b>\n<code>{}</code>".format(result),
+    parse_mode='html')
 
 
 @userge.on_cmd("power", about={
@@ -217,25 +225,26 @@ async def power_(message: Message):
 
     number = message.input_str
     if not number:
-        await message.err(f"```input not found!```")
+        await message.err("```input not found!```")
         return
 
     if "^" not in message.input_str:
-        await message.err(f"```both numbers required in the form of X ^ Y```")
+        await message.err("```both numbers required in the form of X ^ Y```")
         return
 
-    await message.edit(f"```processing...```")
+    await message.edit("```processing...```")
     await asyncio.sleep(1)
 
     number_1, number_2 = message.input_str.split('^', maxsplit=1)
     if not number_2:
-        await message.err(f"```Exponential power required```")
+        await message.err("```Exponential power required```")
         return
     try:
 
         result = math.pow(int(number_1.strip()), int(number_2.strip()))
 
-        await message.edit(f"**OUTPUT:**\n`{int(result)}`")
+        await message.edit("<b>OUTPUT:</b>\n<code>{}</code>".format(result),
+        parse_mode='html')
     except Exception as e:
         await message.err(e)
 
@@ -248,14 +257,15 @@ async def sqrt_(message: Message):
 
     number = message.input_str
     if not number:
-        await message.err(f"```input not found!```")
+        await message.err("```input not found!```")
         return
 
-    await message.edit(f"```processing...```")
+    await message.edit("```processing...```")
     await asyncio.sleep(1)
 
     result = math.sqrt(int(number))
-    await message.edit(f"**OUTPUT:**\n√`{number}` = `{result}`")
+    await message.edit("<b>OUTPUT:</b>\n<code>{}</code>".format(result),
+    parse_mode='html')
 
 
 @userge.on_cmd("sin", about={
@@ -266,14 +276,15 @@ async def sin_(message: Message):
 
     number = message.input_str
     if not number:
-        await message.err(f"```input not found!```")
+        await message.err("```input not found!```")
         return
 
-    await message.edit(f"```processing...```")
+    await message.edit("```processing...```")
     await asyncio.sleep(1)
 
     result = math.sin(float(number))
-    await message.edit(f"**OUTPUT:**\n`sin {number}°` = `{result}°`")
+    await message.edit("<b>OUTPUT:</b>\n<code>{}</code>".format(result),
+    parse_mode='html')
 
 
 @userge.on_cmd("cos", about={
@@ -284,14 +295,15 @@ async def cos_(message: Message):
 
     number = message.input_str
     if not number:
-        await message.err(f"```input not found!```")
+        await message.err("```input not found!```")
         return
 
-    await message.edit(f"```processing...```")
+    await message.edit("```processing...```")
     await asyncio.sleep(1)
 
     result = math.cos(float(number))
-    await message.edit(f"**OUTPUT:**\n`cos {number}°` = `{result}°`")
+    await message.edit("<b>OUTPUT:</b>\n<code>{}</code>".format(result),
+    parse_mode='html')
 
 
 @userge.on_cmd("tan", about={
@@ -302,14 +314,15 @@ async def tan_(message: Message):
 
     number = message.input_str
     if not number:
-        await message.err(f"```input not found!```")
+        await message.err("```input not found!```")
         return
 
-    await message.edit(f"```processing...```")
+    await message.edit("```processing...```")
     await asyncio.sleep(1)
 
     result = math.tan(float(number))
-    await message.edit(f"**OUTPUT:**\n`tan {number}°` = `{result}°`")
+    await message.edit("<b>OUTPUT:</b>\n<code>{}</code>".format(result),
+    parse_mode='html')
 
 
 @userge.on_cmd("deg", about={
@@ -320,14 +333,15 @@ async def deg_(message: Message):
 
     number = message.input_str
     if not number:
-        await message.err(f"```input not found!```")
+        await message.err("```input not found!```")
         return
 
-    await message.edit(f"```processing...```")
+    await message.edit("```processing...```")
     await asyncio.sleep(1)
 
     result = math.degrees(float(number))
-    await message.edit(f"**OUTPUT:**\n`{number}` = `{result}°`")
+    await message.edit("<b>OUTPUT:</b>\n<code>{}</code>".format(result),
+    parse_mode='html')
 
 
 @userge.on_cmd("rad", about={
@@ -338,14 +352,15 @@ async def rad_(message: Message):
 
     number = message.input_str
     if not number:
-        await message.err(f"```input not found!```")
+        await message.err("```input not found!```")
         return
 
-    await message.edit(f"```processing...```")
+    await message.edit("```processing...```")
     await asyncio.sleep(1)
 
     result = math.radians(float(number))
-    await message.edit(f"**OUTPUT:**\n`{number}°` = `{result}`")
+    await message.edit("<b>OUTPUT:</b>\n<code>{}</code>".format(result),
+    parse_mode='html')
 
 
 @userge.on_cmd("log", about={
@@ -356,11 +371,12 @@ async def log_(message: Message):
 
     number = message.input_str
     if not number:
-        await message.err(f"```input not found!```")
+        await message.err("```input not found!```")
         return
 
-    await message.edit(f"```processing...```")
+    await message.edit("```processing...```")
     await asyncio.sleep(1)
 
     result = math.degrees(float(number))
-    await message.edit(f"**OUTPUT:**\n`log{number}` = `{result}`")
+    await message.edit("<b>OUTPUT:</b>\n<code>{}</code>".format(result),
+parse_mode='html')
