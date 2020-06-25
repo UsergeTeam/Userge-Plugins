2d922c4ead3273911347f1dcc0d2f53b022cd831
Krishna-singhal <65902764+rowdy-hero@users.noreply.github.com>
2020-06-25 10:55:09 +0530

    Update figlet.py and calendar.py (#68)
    * Update figlet.py
    
    * Update calendar.py

--- plugins/calendar.py
+++ plugins/calendar.py
@@ -3,16 +3,16 @@ import calendar
 from userge import userge, Message
 
 
-@userge.on_cmd("calendar", about={
+@userge.on_cmd("cal", about={
     'header': "Print calendar of any month of any year.",
-    'usage': "{tr}calendar [ year | month]",
-    'examples': "{tr}calendar 2020 | 6"})
-async def calendar_(message: Message):
+    'usage': "{tr}cal [ year | month]",
+    'examples': "{tr}cal 2020 | 6"})
+async def cal_(message: Message):
 
     if not message.input_str:
         await message.err(
             "I don't found any input text"
-            "For more help do .help .calendar")
+            "For more help do .help .cal")
         return
     if '|' not in message.input_str:
         await message.err("both year and month required!")
