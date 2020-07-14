b79a16d08f6f42536279ab5ca8692e3cb2b15dbf
code-rgb <62891774+code-rgb@users.noreply.github.com>
2020-07-14 18:59:36 +0530

    added a function to remove unsafe emoji
    Now it works with text containing Emoji !

--- plugins/animesticker.py
+++ plugins/animesticker.py
@@ -3,8 +3,30 @@
 # by @krishna_singhal
 
 import random
-
+from random import choice
 from userge import userge, Message
+import re
+from re import sub
+
+EMOJI_PATTERN = re.compile(
+    "["
+    "\U0001F1E0-\U0001F1FF"  # flags (iOS)
+    "\U0001F300-\U0001F5FF"  # symbols & pictographs
+    "\U0001F600-\U0001F64F"  # emoticons
+    "\U0001F680-\U0001F6FF"  # transport & map symbols
+    "\U0001F700-\U0001F77F"  # alchemical symbols
+    "\U0001F780-\U0001F7FF"  # Geometric Shapes Extended
+    "\U0001F800-\U0001F8FF"  # Supplemental Arrows-C
+    "\U0001F900-\U0001F9FF"  # Supplemental Symbols and Pictographs
+    "\U0001FA00-\U0001FA6F"  # Chess Symbols
+    "\U0001FA70-\U0001FAFF"  # Symbols and Pictographs Extended-A
+    "\U00002702-\U000027B0"  # Dingbats 
+    "]+")
+
+
+def deEmojify(inputString: str) -> str:
+    """Remove emojis and other non-safe characters from string"""
+    return re.sub(EMOJI_PATTERN, '', inputString)
 
 
 @userge.on_cmd("sticker", about={
@@ -41,7 +63,7 @@ async def anime_sticker(message: Message):
     try:
         stickers = await userge.get_inline_bot_results(
             "stickerizerbot",
-            f"#{animus}{text}"
+            f"#{animus}{deEmojify(text)}"
         )
         await userge.send_inline_bot_result(
             chat_id=message.chat.id,
