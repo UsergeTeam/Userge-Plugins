215d3b2d587ec7538f26f37e8b4b87e4c407bcf6
Krishna-singhal <65902764+rowdy-hero@users.noreply.github.com>
2020-07-14 20:14:26 +0530

    Update animesticker.py
    Adding feature to remove emoji in text string

--- plugins/animesticker.py
+++ plugins/animesticker.py
@@ -3,8 +3,29 @@
 # by @krishna_singhal
 
 import random
-
+import re
 from userge import userge, Message
+
+
+EMOJI_PATTERN = re.compile(
+    "["
+    "\U0001F1E0-\U0001F1FF"  # flags (iOS)
+    "\U0001F300-\U0001F5FF"  # symbols & pictographs
+    "\U0001F600-\U0001F64F"  # emoticons
+    "\U0001F680-\U0001F6FF"  # transport & map symbols
+    "\U0001F700-\U0001F77F"  # alchemical symbols
+    "\U0001F780-\U0001F7FF"  # Geometric Shapes Extended
+    "\U0001F800-\U0001F8FF"  # Supplemental Arrows-C
+    "\U0001F900-\U0001F9FF"  # Supplemental Symbols and Pictographs
+    "\U0001FA00-\U0001FA6F"  # Chess Symbols
+    "\U0001FA70-\U0001FAFF"  # Symbols and Pictographs Extended-A
+    "\U00002702-\U000027B0"  # Dingbats
+    "]+")
+
+
+def deEmojify(inputString: str) -> str:
+    """Remove emojis and other non-safe characters from string"""
+    return re.sub(EMOJI_PATTERN, '', inputString)
 
 
 @userge.on_cmd("sticker", about={
@@ -17,7 +38,7 @@ from userge import userge, Message
              "{tr}sticker [flags] [text | reply to message]",
     'examples': [
         "{tr}sticker Hello boys and girls",
-        "{tr}sticker [flags] Hello boys and girls"]}, allow_via_bot=False)
+        "{tr}sticker [flags] Hello boys and girls"]}, allow_via_bot=True)
 async def anime_sticker(message: Message):
     """ Creates random anime sticker! """
     if message.reply_to_message:
@@ -25,7 +46,7 @@ async def anime_sticker(message: Message):
     else:
         text = message.filtered_input_str
     if not text:
-        await message.err("```Input not found! ...```")
+        await message.err("```Input not found!...```")
         return
     if '-f' in message.flags:
         k = [20, 32, 33, 40, 41, 42, 58]
@@ -37,11 +58,11 @@ async def anime_sticker(message: Message):
     else:
         k = [1, 3, 7, 9, 13, 22, 34, 35, 36, 37, 43, 44, 45, 52, 53, 55]
         animus = random.choice(k)
-    await message.edit("```Lemme create a sticker ...```")
+    await message.edit("```Lemme create a sticker...```")
     try:
         stickers = await userge.get_inline_bot_results(
             "stickerizerbot",
-            f"#{animus}{text}"
+            f"#{animus}{deEmojify(text)}"
         )
         await userge.send_inline_bot_result(
             chat_id=message.chat.id,
