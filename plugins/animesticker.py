f0ca2d377e667f5b83a7e7d8451c468014df1ee3
Krishna-singhal <65902764+rowdy-hero@users.noreply.github.com>
2020-07-19 17:27:51 +0530

    Removed bot_stamp 😄 (#84)
    * Removed bot_stamp 😄
    
    * Update animesticker.py
    
    * fix bug risk 😈

--- plugins/animesticker.py
+++ plugins/animesticker.py
@@ -41,34 +41,60 @@ def deEmojify(inputString: str) -> str:
         "{tr}sticker [flags] Hello boys and girls"]}, allow_via_bot=False)
 async def anime_sticker(message: Message):
     """ Creates random anime sticker! """
-    if message.reply_to_message:
-        text = message.reply_to_message.text
+    replied = message.reply_to_message
+    args = message.input_str
+    if args:
+        text = args
+    elif replied:
+        text = args if args else replied.text
     else:
-        text = message.filtered_input_str
-    if not text:
         await message.err("```Input not found!...```")
         return
+    await message.edit("```Lemme create a sticker...```")
+    if '-ggl' in message.flags:
+        try:
+            stickers = await userge.get_inline_bot_results(
+                "stickerizerbot",
+                f"#12{deEmojify(text)}")
+            await userge.send_inline_bot_result(
+                chat_id=message.chat.id,
+                query_id=stickers.query_id,
+                result_id=stickers.results[0].id,
+                hide_via=True)
+        except IndexError:
+            await message.edit("```List index out of range```", del_in=3)
+        else:
+            await message.delete()
+            return
     if '-f' in message.flags:
         k = [20, 32, 33, 40, 41, 42, 58]
         animus = random.choice(k)
-    elif '-ggl' in message.flags:
-        animus = 12
     elif '-mock' in message.flags:
         animus = 7
     else:
         k = [1, 3, 7, 9, 13, 22, 34, 35, 36, 37, 43, 44, 45, 52, 53, 55]
         animus = random.choice(k)
-    await message.edit("```Lemme create a sticker...```")
     try:
         stickers = await userge.get_inline_bot_results(
             "stickerizerbot",
             f"#{animus}{deEmojify(text)}"
         )
-        await userge.send_inline_bot_result(
-            chat_id=message.chat.id,
+        saved = await userge.send_inline_bot_result(
+            chat_id="me",
             query_id=stickers.query_id,
-            result_id=stickers.results[0].id
+            result_id=stickers.results[0].id,
+            hide_via=True
         )
-        await message.delete()
+        Sticker = await userge.get_messages("me", int(saved.updates[1].message.id))
+        message_id = replied.message_id if replied else None
+        await userge.send_sticker(
+            chat_id=message.chat.id,
+            sticker=str(Sticker.sticker.file_id),
+            file_ref=str(Sticker.sticker.file_ref),
+            reply_to_message_id=message_id
+        )
+        await saved.delete()
     except IndexError:
         await message.edit("```List index out of range```", del_in=3)
+    else:
+        await message.delete()
