aa0ed4e97002404e46ee5a3ea4e0c2172ff66aaa
rking32 <sljohnwick@gmail.com>
2020-07-20 19:24:15 +0530

    next level github

--- plugins/github.py
+++ plugins/github.py
@@ -2,6 +2,7 @@
 
 Syntax: .github USERNAME
 """
+
 import requests
 
 from userge import userge, Message
@@ -9,41 +10,53 @@ from userge import userge, Message
 
 @userge.on_cmd("github", about={
     'header': "Get info about an GitHub User",
-    'usage': ".github [username]",
-    'examples': ".github cyberboysumanjay"})
+    'flags': {'-l': "repo limit : default to 5"},
+    'usage': ".github [flag] [username]",
+    'examples': [".github cyberboysumanjay", ".github -l5 cyberboysumanjay"]})
 async def fetch_github_info(message: Message):
-    username = message.input_or_reply_str
+    replied = message.reply_to_message
+    username = message.filtered_input_str
+    if replied:
+        username = replied.text
+    if not username:
+        await message.err("invalid input !")
+        return
     url = "https://api.github.com/users/{}".format(username)
-    r = requests.get(url)
-    if r.status_code == 200:
-        b = r.json()
-        avatar_url = b["avatar_url"]
-        html_url = b["html_url"]
-        gh_type = b["type"]
-        name = b["name"]
-        company = b["company"]
-        blog = b["blog"]
-        location = b["location"]
-        bio = b["bio"]
-        followers = b["followers"]
-        following = b["following"]
-        created_at = b["created_at"]
-        await message.client.send_photo(
-            message.chat.id,
-            caption="""Name: [{}]({})
-Type: {}
-Company: {}
-Blog: {}
-Location: {}
-Bio: {}
-Followers: {}
-Following: {}
-Profile Created: {}""".format(
-                name, html_url, gh_type, company, blog, location,
-                bio, followers, following, created_at),
-            photo=avatar_url,
-            disable_notification=True
-        )
+    res = requests.get(url)
+    if res.status_code == 200:
+        await message.edit("`fetching github info ...`")
+        data = res.json()
+        photo = data["avatar_url"]
+        if data['bio']:
+            data['bio'] = data['bio'].strip()
+        repos = []
+        sec_res = requests.get(data["repos_url"])
+        if sec_res.status_code == 200:
+            limit = int(message.flags.get('-l', 5))
+            for repo in sec_res.json():
+                repos.append(f"[{repo['name']}]({repo['html_url']})")
+                limit -= 1
+                if limit == 0:
+                    break
+        template = """
+\b👤 **Name** : [{name}]({html_url})
+🔧 **Type** : `{type}`
+🏢 **Company** : `{company}`
+🔭 **Blog** : {blog}
+📍 **Location** : `{location}`
+📝 **Bio** : __{bio}__
+❤️ **Followers** : `{followers}`
+👁 **Following** : `{following}`
+📊 **Public Repos** : `{public_repos}`
+📄 **Public Gists** : `{public_gists}`
+🔗 **Profile Created** : `{created_at}`
+✏️ **Profile Updated** : `{updated_at}`\n""".format(**data)
+        if repos:
+            template += "🔍 **Some Repos** : " + ' | '.join(repos)
+        await message.client.send_photo(chat_id=message.chat.id,
+                                        caption=template,
+                                        photo=photo,
+                                        disable_notification=True)
         await message.delete()
     else:
         await message.edit("No user found with `{}` username!".format(username))
