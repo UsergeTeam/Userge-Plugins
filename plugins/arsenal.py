f55c74f04bca7168f90b1535dba30dada784f336
Krishna-singhal <65902764+Krishna-Singhal@users.noreply.github.com>
2020-07-21 13:27:32 +0530

    minor fixes (#90)
    * minor fixes
    
    * minor fixes in chat.py
    
    * minor fix in animesticker

--- plugins/arsenal.py
+++ plugins/arsenal.py
@@ -7,7 +7,7 @@ from userge import userge, Message
 @userge.on_cmd("benall", about={
     'header': "Ban All",
     'description': "Bans All Members of a SuperGroup",
-    'usage': "{tr}benall"})
+    'usage': "{tr}benall"}, allow_private=False, only_admins=True)
 async def ban_all(message: Message):
     chat = message.chat.id
     ben_c = 0
@@ -23,7 +23,7 @@ async def ban_all(message: Message):
 @userge.on_cmd("keckall", about={
     'header': "Kick All",
     'description': "Kicks All Members of a SuperGroup",
-    'usage': "{tr}keckall"})
+    'usage': "{tr}keckall"}, allow_private=False, only_admins=True)
 async def keck_all(message: Message):
     chat = message.chat.id
     keck_c = 0
