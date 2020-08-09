3ca03b34a16f4d581328854dd6957fc7b56dbeaf
Krishna-singhal <65902764+Krishna-Singhal@users.noreply.github.com>
2020-08-09 14:06:02 +0530

    some changes in animesticker.py (#116)
    * some changes
    
    * fix import error

--- plugins/animesticker.py
+++ plugins/animesticker.py
@@ -3,29 +3,9 @@
 # by @krishna_singhal
 
 import random
-import re
+
+from userge.utils import demojify
 from userge import userge, Message
-
-
-EMOJI_PATTERN = re.compile(
-    "["
-    "\U0001F1E0-\U0001F1FF"  # flags (iOS)
-    "\U0001F300-\U0001F5FF"  # symbols & pictographs
-    "\U0001F600-\U0001F64F"  # emoticons
-    "\U0001F680-\U0001F6FF"  # transport & map symbols
-    "\U0001F700-\U0001F77F"  # alchemical symbols
-    "\U0001F780-\U0001F7FF"  # Geometric Shapes Extended
-    "\U0001F800-\U0001F8FF"  # Supplemental Arrows-C
-    "\U0001F900-\U0001F9FF"  # Supplemental Symbols and Pictographs
-    "\U0001FA00-\U0001FA6F"  # Chess Symbols
-    "\U0001FA70-\U0001FAFF"  # Symbols and Pictographs Extended-A
-    "\U00002702-\U000027B0"  # Dingbats
-    "]+")
-
-
-def deEmojify(inputString: str) -> str:
-    """Remove emojis and other non-safe characters from string"""
-    return re.sub(EMOJI_PATTERN, '', inputString)
 
 
 @userge.on_cmd("sticker", about={
@@ -48,21 +28,21 @@ async def anime_sticker(message: Message):
     elif replied:
         text = args if args else replied.text
     else:
-        await message.err("```Input not found!...```", del_in=3)
+        await message.err("Input not found!")
         return
     await message.delete()
     if '-ggl' in message.flags:
         try:
             stickers = await userge.get_inline_bot_results(
                 "stickerizerbot",
-                f"#12{deEmojify(text)}")
+                f"#12{demojify(text)}")
             await userge.send_inline_bot_result(
                 chat_id=message.chat.id,
                 query_id=stickers.query_id,
                 result_id=stickers.results[0].id,
                 hide_via=True)
         except IndexError:
-            await message.edit("```List index out of range```", del_in=3)
+            await message.err("List index out of range")
         else:
             await message.delete()
             return
@@ -77,7 +57,7 @@ async def anime_sticker(message: Message):
     try:
         stickers = await userge.get_inline_bot_results(
             "stickerizerbot",
-            f"#{animus}{deEmojify(text)}"
+            f"#{animus}{demojify(text)}"
         )
         saved = await userge.send_inline_bot_result(
             chat_id="me",
@@ -95,4 +75,4 @@ async def anime_sticker(message: Message):
         )
         await saved.delete()
     except IndexError:
-        await message.edit("```List index out of range```", del_in=3)
+        await message.err("List index out of range")
