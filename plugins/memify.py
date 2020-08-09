780f8531ad8a185fefa4b1da086d5a5ed5e61836
Krishna-singhal <65902764+Krishna-Singhal@users.noreply.github.com>
2020-08-09 13:29:22 +0530

    Added Cbutton 😙 (#115)
    * New font for Memify 🙂
    
    * Adding New Font 🙂
    
    * Added Cbutton ☺️
    
    * fixing pep8
    
    * fixing import
    
    * small fixes
    
    * forgot that
    
    Co-authored-by: rking32 <sljohnwick@gmail.com>

--- plugins/memify.py
+++ plugins/memify.py
@@ -65,7 +65,7 @@ async def draw_meme_text(image_path, text):
     img = Image.open(image_path)
     os.remove(image_path)
     i_width, i_height = img.size
-    m_font = ImageFont.truetype("resources/MutantAcademyBB.ttf", int((70 / 640)*i_width))
+    m_font = ImageFont.truetype("resources/MutantAcademyStyle.ttf", int((70 / 640) * i_width))
     if ";" in text:
         upper_text, lower_text = text.split(";")
     else:
