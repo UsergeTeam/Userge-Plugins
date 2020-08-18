47f38845561a9c6604eaa123217d062925eb9d80
Krishna-singhal <65902764+Krishna-Singhal@users.noreply.github.com>
2020-08-18 21:02:01 +0530

    Update ascii.py

--- plugins/ascii.py
+++ plugins/ascii.py
@@ -1,6 +1,17 @@
 """ ASCII Sticker """
 
-# By @Krishna_Singhal
+# The function convert an image to ascii art
+# f: Input filename
+# SC: the horizontal pixel sampling rate. It should be between 0(exclusive) and 1(inclusive). The larger the number, the more details in the output. 
+#   If you want the ascii art output be the same size as input, use ~ 1/ font size width. 
+# GCF: >0. It's an image tuning factor. If GCF>1, the image will look brighter; if 0<GCF<1, the image will look darker.
+# out_f: output filename
+# color1, color2, bgcolor: follow W3C color naming https://www.w3.org/TR/css3-color/#svg-color
+#
+# Copyright 2017, Shanshan Wang, MIT license
+# (C) Author: Krishna-Singhal < https://github.com/krishna-singhal >  <@Krishna_Singhal>
+#
+# All Rights Reserved.
 
 import os
 import numpy as np
