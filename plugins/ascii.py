c31cac22df1ac2ea5ed50bb741a056790c7395db
Krishna-singhal <65902764+Krishna-Singhal@users.noreply.github.com>
2020-08-18 21:11:02 +0530

    fixed Pep8

--- plugins/ascii.py
+++ plugins/ascii.py
@@ -2,9 +2,12 @@
 
 # The function convert an image to ascii art
 # f: Input filename
-# SC: the horizontal pixel sampling rate. It should be between 0(exclusive) and 1(inclusive). The larger the number, the more details in the output. 
-#   If you want the ascii art output be the same size as input, use ~ 1/ font size width. 
-# GCF: >0. It's an image tuning factor. If GCF>1, the image will look brighter; if 0<GCF<1, the image will look darker.
+# SC: the horizontal pixel sampling rate. It should be between 0(exclusive) and 1(inclusive).
+# The larger the number, the more details in the output.
+# If you want the ascii art output be the same size as input, use ~ 1/ font size width.
+# GCF: >0. It's an image tuning factor. If GCF>1, the image will look brighter; if 0<GCF<1,
+# the image will look darker.
+#
 # out_f: output filename
 # color1, color2, bgcolor: follow W3C color naming https://www.w3.org/TR/css3-color/#svg-color
 #
