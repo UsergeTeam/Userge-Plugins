0bd7e25ac5f7b04e764e1446032ad31bf965d523
rking32 <52490534+rking32@users.noreply.github.com>
2020-09-17 01:47:32 +0530

    line too long + missing space

--- plugins/watch.py
+++ plugins/watch.py
@@ -9,7 +9,8 @@ from justwatch import JustWatch, justwatchapi
 from userge import userge, Message
 
 # https://github.com/dawoudt/JustWatchAPI/issues/47#issuecomment-691357371
-justwatchapi.__dict__['HEADER'] = {'User-Agent':'JustWatch client (github.com/dawoudt/JustWatchAPI)'}
+justwatchapi.__dict__['HEADER'] = {
+    'User-Agent': 'JustWatch client (github.com/dawoudt/JustWatchAPI)'}
 
 LOGGER = userge.getLogger(__name__)
 WATCH_COUNTRY = os.environ.get("WATCH_COUNTRY", "IN")
