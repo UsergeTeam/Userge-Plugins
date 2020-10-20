20806b2d9838395c6b945ae30736563d054f5ac8
Krishna-singhal <65902764+Krishna-Singhal@users.noreply.github.com>
2020-10-20 10:46:39 +0530

    Will work after restart

--- plugins/autobio.py
+++ plugins/autobio.py
@@ -11,7 +11,8 @@ from resources.quotes import ENGLISH_QUOTES, HINDI_QUOTES
 from userge import userge, Message, get_collection
 
 BIO_UPDATION = False
-AUTOBIO_TIMEOUT = 86400
+BIO_QUOTES = ENGLISH_QUOTES
+
 USER_DATA = get_collection("CONFIGS")
 
 CHANNEL = userge.getCLogger(__name__)
@@ -30,7 +31,7 @@ async def _init() -> None:
     'usage': "{tr}autobio (for eng)\n{tr}autobio Hi (for hindi)"})
 async def auto_bio(msg: Message):
     """ Auto Update Your Bio """
-    global BIO_UPDATION  # pylint: disable=global-statement
+    global BIO_UPDATION, BIO_QUOTES  # pylint: disable=global-statement
     if BIO_UPDATION:
         if isinstance(BIO_UPDATION, asyncio.Task):
             BIO_UPDATION.cancel()
@@ -43,37 +44,31 @@ async def auto_bio(msg: Message):
             "Auto Bio Updation is **Stopped** Successfully...", log=__name__, del_in=5)
         return
 
-    if not msg.input_str:
-        bio_quotes = ENGLISH_QUOTES
-    elif 'hi' in msg.input_str.lower():
-        bio_quotes = HINDI_QUOTES
+    if 'hi' in msg.input_str.lower():
+        BIO_QUOTES = HINDI_QUOTES
     else:
-        await msg.err("Given Input is Invalid...")
-        return
+        BIO_QUOTES = ENGLISH_QUOTES
 
     USER_DATA.update_one({'_id': 'BIO_UPDATION'},
                          {"$set": {'on': True}}, upsert=True)
     await msg.edit(
         "Auto Bio Updation is **Started** Successfully...", log=__name__, del_in=3)
-    BIO_UPDATION = asyncio.get_event_loop().create_task(autobio_worker(msg, bio_quotes))
+    BIO_UPDATION = asyncio.get_event_loop().create_task(autobio_worker())
 
 
-# @userge.add_task
-async def autobio_worker(msg: Message, bio_quotes: list):
-    quotes = bio_quotes
+@userge.add_task
+async def autobio_worker():
+    quotes = BIO_QUOTES
     while BIO_UPDATION:
-        for k in range(30):
+        for k in range(len(quotes)):
             if not BIO_UPDATION:
                 break
-
             try:
-                await msg.client.update_profile(bio=quotes[k])
-
+                await userge.update_profile(bio=quotes[k])
             except FloodWait as s_c:
                 time.sleep(s_c.x)
                 CHANNEL.log(s_c)
             except Exception as e:
                 LOG.error(e)
-
-            await asyncio.sleep(AUTOBIO_TIMEOUT)
+            await asyncio.sleep(Config.AUTOBIO_TIMEOUT)
             await CHANNEL.log("Updating Next Quote...")
