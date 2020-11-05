ff1c24dab1ab3900ba1bd4589ffc767271d5baf4
rking32 <sljohnwick@gmail.com>
2020-11-05 10:41:35 +0530

    some tweaks

--- plugins/autobio.py
+++ plugins/autobio.py
@@ -8,9 +8,10 @@ import asyncio
 from pyrogram.errors import FloodWait
 
 from resources.quotes import ENGLISH_QUOTES, HINDI_QUOTES
-from userge import userge, Message, Config, get_collection
+from userge import userge, Message, get_collection
 
 BIO_UPDATION = False
+AUTOBIO_TIMEOUT = 300
 BIO_QUOTES = ENGLISH_QUOTES
 
 USER_DATA = get_collection("CONFIGS")
@@ -20,13 +21,13 @@ LOG = userge.getLogger(__name__)
 
 
 async def _init() -> None:
-    global BIO_UPDATION  # pylint: disable=global-statement
+    global BIO_UPDATION, AUTOBIO_TIMEOUT  # pylint: disable=global-statement
     data = await USER_DATA.find_one({'_id': 'BIO_UPDATION'})
     if data:
         BIO_UPDATION = data['on']
     b_t = await USER_DATA.find_one({'_id': 'AUTOBIO_TIMEOUT'})
     if b_t:
-        Config.AUTOBIO_TIMEOUT = b_t['data']
+        AUTOBIO_TIMEOUT = b_t['data']
 
 
 @userge.on_cmd("autobio", about={
@@ -56,7 +57,7 @@ async def auto_bio(msg: Message):
                          {"$set": {'on': True}}, upsert=True)
     await msg.edit(
         "Auto Bio Updation is **Started** Successfully...", log=__name__, del_in=3)
-    BIO_UPDATION = asyncio.get_event_loop().create_task(autobio_worker())
+    BIO_UPDATION = asyncio.get_event_loop().create_task(_autobio_worker())
 
 
 @userge.on_cmd("sabto", about={
@@ -65,12 +66,13 @@ async def auto_bio(msg: Message):
     'examples': "{tr}sabto 500"})
 async def set_bio_timeout(message: Message):
     """ set auto bio timeout """
+    global AUTOBIO_TIMEOUT  # pylint: disable=global-statement
     t_o = int(message.input_str)
     if t_o < 60:
         await message.err("too short! (minimum 60 sec)")
         return
     await message.edit("`Setting auto bio timeout...`")
-    Config.AUTOBIO_TIMEOUT = t_o
+    AUTOBIO_TIMEOUT = t_o
     await USER_DATA.update_one(
         {'_id': 'AUTOBIO_TIMEOUT'}, {"$set": {'data': t_o}}, upsert=True)
     await message.edit(
@@ -81,23 +83,23 @@ async def set_bio_timeout(message: Message):
 async def view_bio_timeout(message: Message):
     """ view bio timeout """
     await message.edit(
-        f"`Profile picture will be updated after {Config.AUTOBIO_TIMEOUT} seconds!`",
+        f"`Profile picture will be updated after {AUTOBIO_TIMEOUT} seconds!`",
         del_in=5)
 
 
 @userge.add_task
-async def autobio_worker():
-    quotes = BIO_QUOTES
+async def _autobio_worker():
     while BIO_UPDATION:
-        for k in range(len(quotes)):
+        for quote in BIO_QUOTES:
             if not BIO_UPDATION:
                 break
             try:
-                await userge.update_profile(bio=quotes[k])
+                await userge.update_profile(bio=quote)
             except FloodWait as s_c:
+                LOG.warn(s_c)
                 time.sleep(s_c.x)
-                CHANNEL.log(s_c)
-            except Exception as e:
-                LOG.error(e)
-            await asyncio.sleep(Config.AUTOBIO_TIMEOUT)
+                await CHANNEL.log(s_c)
+            except Exception as e_x:  # pylint: disable=broad-except
+                LOG.error(e_x)
+            await asyncio.sleep(AUTOBIO_TIMEOUT)
             await CHANNEL.log("Updating Next Quote...")
