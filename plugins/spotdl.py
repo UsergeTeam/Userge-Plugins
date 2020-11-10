66dac873bbd9d57127aa55e2ce30892e79019301
rking32 <sljohnwick@gmail.com>
2020-11-10 21:42:42 +0530

    some fixes

--- plugins/spotdl.py
+++ plugins/spotdl.py
@@ -14,54 +14,25 @@ TEMP_DIR = "spotdl/"
     'header': "Spotify Downloader",
     'description': "Download Songs via Spotify Links"
                    " or just by giving song names. ",
-    'usage': "{tr}spotdl [Spotify Link or Song Name]|[Quality (optional)]",
-    'examples': "{tr}spotdl https://open.spotify.com/track/0Cy7wt6IlRfBPHXXjmZbcP|flac"})
+    'usage': "{tr}spotdl [Spotify Link or Song Name]",
+    'examples': "{tr}spotdl https://open.spotify.com/track/0Cy7wt6IlRfBPHXXjmZbcP"})
 async def spotify_dl(message: Message):
     if not os.path.exists(TEMP_DIR):
         os.makedirs(TEMP_DIR)
-    await message.edit("Checking? 🧐😳🤔🤔")
-    cmd = ''
-    link = ''
-    song_n = ''
-    quality = "mp3"
-    if "|" in message.input_str:
-        input_, quality = message.input_str.split("|")
-        if 'spotify.com' in input_:
-            await message.edit("Link, Hmm, Make sure to give valid one")
-            link = input_
-        else:
-            await message.edit("🤔Song? Searching...")
-            song_n = input_
+    song_or_link = message.input_str
+    await message.edit(f"`Downloading {song_or_link} ...`")
+    cmd = f"cd {TEMP_DIR} && spotdl {song_or_link}"
+    runn = await Term.execute(cmd)
+    while not runn.finished:
+        await asyncio.sleep(1)
+        await message.try_to_edit(f"<code>{runn.read_line}</code>")
+    if len(os.listdir(TEMP_DIR)) <= 1:
+        await message.err("Download Failed.")
     else:
-        input_ = message.input_str
-        if 'spotify.com' in input_:
-            await message.edit("Link, Hmm, Make sure to gave valid one")
-            link = input_
-        else:
-            await message.edit("🤔Song? Searching....")
-            song_n = input_
-
-    if song_n or link:
-        if 'track/' in link:
-            song_n = link
-        if not song_n:
-            await message.edit("Selling Brain is not yet Legalized")
-            return
-        await message.edit("Downloading")
-        quality = quality.strip()  # Just for Precautions 🤷‍♂
-        cmd = f"cd {TEMP_DIR} && spotdl {song_n}"
-    if cmd:
-        runn = await Term.execute(cmd)
-        while not runn.finished:
-            await asyncio.sleep(0.5)
-            await message.try_to_edit(f"<code>{runn.read_line}</code>")
-        if len(os.listdir(TEMP_DIR)) <= 1:
-            await message.err("Download Failed.")
-        else:
-            await message.delete()
-            for track in os.listdir(TEMP_DIR):
-                if track.startswith("Temp"):
-                    continue
-                track_loc = TEMP_DIR + track
-                await audio_upload(message, Path(track_loc), True)
+        await message.delete()
+        for track in os.listdir(TEMP_DIR):
+            if track.startswith("Temp"):
+                continue
+            track_loc = TEMP_DIR + track
+            await audio_upload(message, Path(track_loc), True)
     shutil.rmtree(TEMP_DIR, ignore_errors=True)
