440ee8ec8c8e6b970d5f5ab3e7937a3c2f6023cf
rking32 <sljohnwick@gmail.com>
2020-11-14 01:15:01 +0530

    fix zippyshare

--- plugins/zippyshare.py
+++ plugins/zippyshare.py
@@ -3,12 +3,6 @@
 # plugin by @aryanvikash
 
 import re
-# import json
-import time
-try:
-    from urllib.parse import unquote
-except ImportError:
-    from urllib import unquote
 
 import requests
 
@@ -25,85 +19,36 @@ async def zippyshare(message: Message):
     await message.edit("`Generating url ....`")
     try:
         direct_url, fname = await _generate_zippylink(url)
-        await message.edit(
-            f"Original : {url}\n\nFilename : `{fname}`\n\nDirect link : {direct_url}")
+        await message.edit(f"**Original** : {url}\n**FileName** : `{fname}`\n"
+                           f"**DirectLink** : {direct_url}\n\n"
+                           "**[HINT]** : use `.download [directLink]`",
+                           disable_web_page_preview=True)
     except Exception as z_e:  # pylint: disable=broad-except
         await message.edit(f"`{z_e}`")
 
 
-# From Here script part starts
-
-# def _decrypt_dlc(abs):
-#     # Thank you, dcrypt owner(s).
-#     url = "http://dcrypt.it/decrypt/paste"
-#     r = s.post(url, data={
-#         'content': open(abs)
-#     }
-#     )
-#     r.raise_for_status()
-#     j = json.loads(r.text)
-#     if not j.get('success'):
-#         raise Exception(j)
-#     return j['success']['links']
-
-
-def _check_url(url):
-    regex = r'https://www(\d{1,3}).zippyshare.com/v/([a-zA-Z\d]{8})/file.html'
-    match = re.match(regex, url)
-    if match:
-        return match.group(1), match.group(2)
-    raise ValueError("Invalid URL: " + str(url))
-
-
-def _extract(ses, url, server, id_):
-    regex = (
-        r'document.getElementById\(\'dlbutton\'\).href = "/d/'
-        r'([a-zA-Z\d]{8})/" \+ \((\d*) % (\d*) \+ (\d*) % '
-        r'(\d*)\) \+ "/(.*)";'
-    )
-    for _ in range(3):
-        res = ses.get(url)
-        if res.status_code != 500:
-            break
-        time.sleep(1)
-    res.raise_for_status()
-    meta = re.search(regex, res.text)
-    if not meta:
-        raise Exception('Failed to get file URL. Down?')
-    num_1 = int(meta.group(2))
-    num_2 = int(meta.group(3))
-    num_3 = int(meta.group(4))
-    num_4 = int(meta.group(5))
-    enc_fname = meta.group(6)
-    final_num = num_1 % num_2 + num_3 % num_4
-    file_url = "https://www{}.zippyshare.com/d/{}/{}/{}".format(server,
-                                                                id_,
-                                                                final_num,
-                                                                enc_fname)
-    fname = unquote(enc_fname)
-    return file_url, fname
-
-
-# def _get_file(ref, url):
-#     s.headers.update({
-#         'Range': "bytes=0-",
-#         'Referer': ref
-#     })
-#     r = s.get(url, stream=True)
-#     del s.headers['Range']
-#     del s.headers['Referer']
-#     r.raise_for_status()
-#     length = int(r.headers['Content-Length'])
-#     return r, length
+_REGEX_LINK = r'https://www(\d{1,3}).zippyshare.com/v/(\w{8})/file.html'
+_REGEX_RESULT = (r'var a = (\d+);[\s\S]+document.getElementById\(\'dlbutton\'\).href'
+                 r' = "/d/\w{8}/.+/(.*)";')
+_HEADERS = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                          "AppleWebKit/537.36 (KHTML, like Gecko) Chrome"
+                          "/75.0.3770.100 Safari/537.36"}
 
 
 @pool.run_in_thread
 def _generate_zippylink(url):
-    ses = requests.Session()
-    ses.headers.update({
-        'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-                      "AppleWebKit/537.36 (KHTML, like Gecko) Chrome"
-                      "/75.0.3770.100 Safari/537.36"
-    })
-    server, id_ = _check_url(url)
-    return _extract(ses, url, server, id_)
+    session = requests.Session()
+    session.headers.update(_HEADERS)
+    with session as ses:
+        match = re.match(_REGEX_LINK, url)
+        if not match:
+            raise ValueError("Invalid URL: " + str(url))
+        server, id_ = match.group(1), match.group(2)
+        res = ses.get(url)
+        res.raise_for_status()
+        match = re.search(_REGEX_RESULT, res.text)
+        if not match:
+            raise ValueError("Invalid Response!")
+        val, name = int(match.group(1)), match.group(2)
+        d_l = "https://www{}.zippyshare.com/d/{}/{}/{}".format(server, id_, val**3+3, name)
+    return d_l
