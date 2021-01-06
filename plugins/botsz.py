15076d9ae5dcfc05e983c31a868345b12834ba1d
Albert Einstein <73480087+AlbertEinsteinTG@users.noreply.github.com>
2021-01-06 13:06:44 +0530

    A Plugin To Check The Bots
    A Plugin To Check Whether All The Bot Are Alive Or Not. Pretty Useful For Channel Admins Or Owner To Check Whether There Bots Are Alive....

---
+++ plugins/botsz.py
@@ -0,0 +1,48 @@
+#Professor | Albert Einstein
+#@TheUnusualPsychopath | @Albert_Einetin_TG
+#@TrojanzHEX | @CrazyBotsz
+
+import os
+import time
+import pytz
+import pyrogram
+import asyncio
+from datetime import datetime
+from userge import userge, Message
+
+UpdatesChannel = os.environ.get("UPDATES_CHANNEL")
+Botsz = [i.strip() for i in os.environ.get("BOTSZ").split(',')]
+
+@userge.on_cmd("balive", about={
+    'header': "Pings All Defined Bots",
+    'description': "<b>Ping and Updates The Status Of All Defined Bots In 'BOTSZ' var</b>\n\n"
+                   "<b>Available Vars</b>:\n\n<i><code>UPDATES_CHANNEL</code> : Provide Your Channel Name With @"
+
+                   "<code>BOTSZ</code> : Define All Your Bot's Username With Out @ And Seperate Each With ','\n\n"
+})
+async def bots(message: Message):
+    first_msg = f"<b>Bots Status @{UpdatesChannel}\n°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°</b>\n\n"
+    reply = await message.reply_text(first_msg,parse_mode="html")
+    Listed = Botsz
+    for bot in Listed:
+        checking = f"<b>⚡ @{bot} Status : Checking...⌛</b>\n\n"
+        first_msg += checking
+        await reply.edit_text(first_msg,parse_mode="html")
+        snt = await userge.send_message(bot, '/start')
+        time.sleep(5)
+        msg = await userge.get_history(bot, 1)
+        if snt.message_id == msg[0].message_id:
+            nice = f"<b>⚡ @{bot} Status : ❎</b>\n\n"
+        else:
+            nice = f"<b>⚡ @{bot} Status : ✅</b>\n\n"
+        first_msg = first_msg.replace(checking, nice)
+        await reply.edit_text(first_msg,parse_mode="html")
+        await userge.read_history(bot)
+    tz = pytz.timezone('Asia/Kolkata')
+    time_now  = datetime.utcnow().astimezone(tz=tz).strftime("%I:%M %p - %d %B %Y")
+    first_msg += f"<code>[Updated on : {time_now}]</code>"
+    await reply.edit_text(first_msg,parse_mode="html")
+
+    
+#@CrazyBotsz
+#@TheUnusualPsychopath
