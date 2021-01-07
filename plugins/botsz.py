336c4123469ffae2f2ffe159cec232604313c0bb
Krishna-singhal <65902764+Krishna-Singhal@users.noreply.github.com>
2021-01-07 14:25:30 +0530

    fixed

--- plugins/botsz.py
+++ plugins/botsz.py
@@ -8,28 +8,31 @@ import pytz
 from datetime import datetime
 from userge import userge, Message
 
-UpdatesChannel = os.environ.get("UPDATES_CHANNEL")
-Botsz = [i.strip() for i in os.environ.get("BOTSZ").split(',')]
+UpdatesChannel = os.environ.get("UPDATES_CHANNEL", None)
 
 
 @userge.on_cmd("balive", about={
     'header': "Pings All Defined Bots",
-    'description': "<b>Ping and Updates The Status Of All Defined Bots In 'BOTSZ' var</b>\n\n"
-
+    'description': "<b>Ping and Updates The Status Of "
+                   "All Defined Bots In UpdatesChannel.</b>\n\n"
                    "Available Vars:\n\n"
-
-                   "UPDATES_CHANNEL : Provide Your Channel Name With @\n\n"
-
-                   "BOTSZ : Define All Your Bot's Username With Out @ And Seperate Each With ','"
+                   "UPDATES_CHANNEL : Provide Your Channel id or Username."
 })
 async def bots(message: Message):
     first_msg = f"<b>Bots Status @{UpdatesChannel}\n°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°</b>\n\n"
-    reply = await message.reply_text(first_msg, parse_mode="html")
-    Listed = Botsz
-    for bot in Listed:
+    await message.edit(first_msg, parse_mode="html")
+    Bot_List = []
+    if UpdatesChannel:
+        async for bots in userge.get_chat_members(
+            chat="UpdatesChannel", filter="bots"
+        ):
+            Bot_List.append(bots.id)
+    if not Bot_List:
+        return await message.edit(f"Bots not found in {UpdatesChannel}!")
+    for bot in Bot_List:
         checking = f"<b>⚡ @{bot} Status : Checking...⌛</b>\n\n"
         first_msg += checking
-        await reply.edit_text(first_msg, parse_mode="html")
+        await message.edit(first_msg, parse_mode="html")
         snt = await userge.send_message(bot, '/start')
         time.sleep(5)
         msg = await userge.get_history(bot, 1)
@@ -38,12 +41,12 @@ async def bots(message: Message):
         else:
             nice = f"<b>⚡ @{bot} Status : ✅</b>\n\n"
         first_msg = first_msg.replace(checking, nice)
-        await reply.edit_text(first_msg, parse_mode="html")
+        await message.edit(first_msg, parse_mode="html")
         await userge.read_history(bot)
     tz = pytz.timezone('Asia/Kolkata')
     time_now = datetime.utcnow().astimezone(tz=tz).strftime("%I:%M %p - %d %B %Y")
     first_msg += f"<code>[Updated on : {time_now}]</code>"
-    await reply.edit_text(first_msg, parse_mode="html")
+    await message.edit(first_msg, parse_mode="html")
 
 
 # @CrazyBotsz
