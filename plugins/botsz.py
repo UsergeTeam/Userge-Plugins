1ed8c8e5cc4c32fd283525cea34870b925cff391
Krishna-singhal <65902764+Krishna-Singhal@users.noreply.github.com>
2021-01-07 15:05:51 +0530

    pep8

--- plugins/botsz.py
+++ plugins/botsz.py
@@ -18,14 +18,14 @@ from userge import userge, Message
     }, allow_via_bot=False
 )
 async def bots(message: Message):
-    msg = "<b>Bots Status\n°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°</b>\n\n"
+    _msg = "<b>Bots Status\n°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°</b>\n\n"
     await message.edit(first_msg, parse_mode="html")
     if not message.input_str:
         return await message.edit("Bots not found!")
     Bot_List = [bot.strip() for bot in message.input_str.split('\n') if bot.strip()]
     for bot in Bot_List:
         checking = f"<b>⚡ {bot} Status : Checking...⌛</b>\n\n"
-        msg += checking
+        _msg += checking
         await message.edit(first_msg, parse_mode="html")
         snt = await userge.send_message(bot, '/start')
         time.sleep(5)
@@ -34,7 +34,7 @@ async def bots(message: Message):
             nice = f"<b>⚡ {bot} Status : ❎</b>\n\n"
         else:
             nice = f"<b>⚡ {bot} Status : ✅</b>\n\n"
-        first_msg = first_msg.replace(checking, nice)
+        _msg = _msg.replace(checking, nice)
         await message.edit(first_msg, parse_mode="html")
         await userge.read_history(bot)
     tz = pytz.timezone('Asia/Kolkata')
