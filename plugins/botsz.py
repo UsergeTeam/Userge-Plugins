0e4b828d56863ec88f5d3ebc05d8b8f920f5339b
ItsPahul <60101949+Pahul124@users.noreply.github.com>
2021-01-08 01:04:14 +0530

    fixed typos (#163)

--- plugins/botsz.py
+++ plugins/botsz.py
@@ -11,9 +11,9 @@ from userge import userge, Message
 @userge.on_cmd(
     "balive", about={
         'header': "Pings All Defined Bots",
-        'description': "<b>Ping and All bots and check their status.</b>\n\n"
+        'description': "<b>Pings All bots you mention and check their status.</b>\n\n"
                        "[NOTE]: you can pass multiple ids, seprate them via new line",
-        'usage': "{tr}belive [bot id/username]"}, allow_via_bot=False)
+        'usage': "{tr}balive [bot id/username]"}, allow_via_bot=False)
 async def bots(message: Message):
     _msg = "<b>Bots Status\n°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°</b>\n\n"
     await message.edit(_msg, parse_mode="html")
