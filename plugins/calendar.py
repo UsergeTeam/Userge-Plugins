00bd918f955aa887d14c9a9b1e2f0b1d4931b569
Aakash Singh <46787056+sainAk@users.noreply.github.com>
2021-02-05 18:59:49 +0530

    .cal shows current months' calender when nothing is specified (#169)

--- plugins/calendar.py
+++ plugins/calendar.py
@@ -1,18 +1,23 @@
 import calendar  # pylint: disable=W0406
+from datetime import datetime
 
 from userge import userge, Message
 
 
 @userge.on_cmd("cal", about={
     'header': "Print calendar of any month of any year.",
-    'usage': "{tr}cal [ year | month]",
+    'usage': "{tr}cal\n{tr}cal [ year | month]",
     'examples': "{tr}cal 2020 | 6"})
 async def cal_(message: Message):
 
     if not message.input_str:
-        await message.err(
-            "I don't found any input text"
-            "For more help do .help .cal")
+        await message.edit("`Searching...`")
+        try:
+            today = datetime.today()
+            input_ = calendar.month(today.year, today.month)
+            await message.edit(f"```{input_}```")
+        except Exception as e:
+            await message.err(e)
         return
     if '|' not in message.input_str:
         await message.err("both year and month required!")
