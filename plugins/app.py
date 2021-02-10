03388dcf4c4b60fcd9ab20fb57948477f0c0fe13
Tomi Prasetio <69198533+TomyPrs@users.noreply.github.com>
2021-02-10 17:50:31 +0700

    app.py: Add magisk scraper
    Signed-off-by: TomyPrs <tomy.prs25@gmail.com>
    
    magisk: add missing credits
    
    thanks for notice me @code-rgb
    
    Signed-off-by: TomyPrs <tomy.prs25@gmail.com>

--- plugins/app.py
+++ plugins/app.py
@@ -5,6 +5,7 @@
 # By - @kirito6969
 
 import bs4
+import aiohttp
 import requests
 
 from userge import userge, Message
@@ -44,3 +45,47 @@ async def app(message: Message):
         await message.edit("No result found in search. Please enter **Valid app name**")
     except Exception as err:
         await message.err(err)
+
+
+# Android user may need this
+# Credits : Telegram-Paperplane & UserindoBot Team
+@userge.on_cmd(
+    'magisk',
+    about={
+        'header': "Fetch all magisk release from source.",
+        'usage': "{tr}magisk",
+    },
+)
+async def magisk(message: Message):
+    """ Scrap all magisk version from source. """
+    magisk_dict = {
+        'Stable': "https://raw.githubusercontent.com/topjohnwu/magisk_files/master/stable.json",
+        'Beta': "https://raw.githubusercontent.com/topjohnwu/magisk_files/master/beta.json",
+        'Canary': "https://raw.githubusercontent.com/topjohnwu/magisk_files/canary/canary.json",
+    }
+    releases = "**Latest Magisk Releases:**\n\n"
+    async with aiohttp.ClientSession() as session:
+        for name, release_url in magisk_dict.items():
+            async with session.get(release_url) as res:
+                data = await res.json(content_type="text/plain")
+                if name == "Canary":
+                    data['magisk']['link'] = (
+                        "https://github.com/topjohnwu/magisk_files/raw/canary/"
+                        + data['magisk']['link']
+                    )
+                    data['app']['link'] = (
+                        "https://github.com/topjohnwu/magisk_files/raw/canary/"
+                        + data['app']['link']
+                    )
+                    data['uninstaller']['link'] = (
+                        "https://github.com/topjohnwu/magisk_files/raw/canary/"
+                        + data['uninstaller']['link']
+                    )
+
+                releases += (
+                    f"**{name}**:\n"
+                    f"°[ ZIP - v{data['magisk']['version']}]({data['magisk']['link']})\n"
+                    f"° **[APK v{data['app']['version']}]({data['app']['link']})**\n"
+                    f"° **[Uninstaller]({data['uninstaller']['link']})**\n\n"
+                )
+        await message.edit(releases)
