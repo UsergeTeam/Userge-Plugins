31bdc065d24fd090d57362ed725a8c5f8a147db0
rking32 <52490534+rking32@users.noreply.github.com>
2021-02-10 09:55:56 +0530

    fix usage.py

--- plugins/usage.py
+++ plugins/usage.py
@@ -16,7 +16,7 @@ async def usage(message: Message):
     useragent = ('Mozilla/5.0 (Linux; Android 10; SM-G975F) '
                  'AppleWebKit/537.36 (KHTML, like Gecko) '
                  'Chrome/80.0.3987.149 Mobile Safari/537.36')
-    u_id = Config.HEROKU_APP.account().id
+    u_id = Config.HEROKU_APP.owner.id
     headers = {
         'User-Agent': useragent,
         'Authorization': f'Bearer {Config.HEROKU_API_KEY}',
