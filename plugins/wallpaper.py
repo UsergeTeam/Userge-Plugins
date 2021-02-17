8d996e4a94141438008f722646bcf8ac2c77038b
Krishna-singhal <65902764+Krishna-Singhal@users.noreply.github.com>
2021-02-17 19:31:17 +0530

    rebuild wallpaper (#184) and closes #153
    * rebuild wallpaper
    
    * fixed deepsource #153

--- plugins/wallpaper.py
+++ plugins/wallpaper.py
@@ -1,127 +1,73 @@
-"""Wallpaper Module"""
-from userge import userge, Message, Config
-from bs4 import BeautifulSoup as soup
-import requests
-from random import randint, choice
+""" Wallpaper Module """
+
 import os
-import asyncio
+import wget
+import shutil
+import requests
+
+from pyrogram.types import InputMediaPhoto, InputMediaDocument
 from PIL import Image
 
-down_p = str(Config.DOWN_PATH.rstrip('/'))
-
-
-async def dlimg(link):
-    e = requests.get(link).content
-    paea = 'donno.{}'.format(link.split('.')[-1])
-    path_i = os.path.join(down_p, paea)
-    with open(path_i, 'wb') as k:
-        k.write(e)
-    return path_i
-
-
-async def walld(strin: str):
-    if len(strin.split()) > 1:
-        strin = '+'.join(strin.split())
-    url = 'https://wall.alphacoders.com/search.php?search='
-    none_got = ['https://wall.alphacoders.com/finding_wallpapers.php']
-    none_got.append('https://wall.alphacoders.com/search-no-results.php')
-    page_link = 'https://wall.alphacoders.com/search.php?search={}&page={}'
-    resp = requests.get(f'{url}{strin}')
-    if resp.url in none_got:
-        return False
-    if 'by_category.php' in resp.url:
-        page_link = str(resp.url).replace('&amp;', '') + '&page={}'
-        check_link = True
-    else:
-        check_link = False
-    resp = soup(resp.content, 'lxml')
-    try:
-        page_num = resp.find('div', {'class': 'visible-xs'})
-        page_num = page_num.find('input', {'class': 'form-control'})
-        page_num = int(page_num['placeholder'].split(' ')[-1])
-    except Exception:
-        page_num = 1
-    n = randint(1, page_num)
-    if page_num != 1:
-        if check_link:
-            resp = requests.get(page_link.format(n))
-        else:
-            resp = requests.get(page_link.format(strin, n))
-        resp = soup(resp.content, 'lxml')
-    a_s = resp.find_all('a')
-    list_a_s = []
-    tit_links = []
-    r = ['thumb', '350', 'img', 'big.php?i', 'src', 'title']
-    for a_tag in a_s:
-        if all(d in str(a_tag) for d in r):
-            list_a_s.append(a_tag)
-    try:
-        for df in list_a_s:
-            imgi = df.find('img')
-            li = str(imgi['src']).replace('thumb-350-', '')
-            titl = str(df['title']).replace('|', '')
-            titl = titl.replace('  ', '')
-            titl = titl.replace('Image', '')
-            titl = titl.replace('HD', '')
-            titl = titl.replace('Wallpaper', '')
-            titl = titl.replace('Background', '')
-            p = (li, titl)
-            tit_links.append(p)
-    except Exception:
-        pass
-    del list_a_s
-    if len(tit_links) != 0:
-        tit_link = choice(tit_links)
-    else:
-        return False
-    return tit_link
+from userge import userge, Message, pool
 
 
 @userge.on_cmd("wall", about={
     'header': "Search Wallpaper",
-    'description': '''Search and Download Hd Wallpaper from
-alphacoders and upload to Telegram''',
-    'usage': "{tr}wall [Query], [Second Query]",
-    'examples': "{tr}wall luffy",
-    'Another Example': "`{tr}wall Luffy, Naruto`"
-})
-async def idk_sir(message: Message):
-    if not os.path.isdir(down_p):
-        os.makedirs(down_p)
-    cat_id = message.chat.id
-    if message.input_str:
-        qu = message.input_str
-        for q in qu.split(','):
-            q = q.strip()
-            await message.edit(f'**Processing...**\n**Searching for **`{q}`')
-            try:
-                link = await walld(str(q))
-            except Exception as e:
-                await message.edit(e)
-                return
-            if link:
-                idl = await dlimg(link[0])
-                if link[0].endswith('png'):
-                    im = Image.open(idl)
-                    os.remove(idl)
-                    idl = idl.replace('png', 'jpeg')
-                    im = im.convert('RGB')
-                    im.save(idl, 'jpeg')
-                await message.edit('**Uploading...**')
-                if not len(link[1].split()) < 11:
-                    capo = '**' + ' '.join(link[1].split()[:11]) + '**'
-                else:
-                    capo = '**' + link[1] + '**'
-                try:
-                    await message.client.send_photo(cat_id, idl, caption=capo)
-                    await message.client.send_document(cat_id, idl)
-                    os.remove(idl)
-                except Exception as e:
-                    await message.edit(e)
-            else:
-                await message.edit('**Result Not Found**')
-        await asyncio.sleep(3)
-        await message.delete()
+    'flags': {
+        '-l': "Limit of Wallpapers",
+        '-doc': "Send as Documents (Recommended)"
+    },
+    'description': 'Search and Download Hd Wallpaper from Unsplash and upload to Telegram',
+    'usage': "{tr}wall [Query]",
+    'examples': "{tr}wall luffy"})
+async def wall_(msg: Message):
+
+    if os.path.exists("wallpapers/"):
+        shutil.rmtree("wallpapers/", ignore_errors=True)
+
+    limit = min(int(msg.flags.get('-l', 8)), 10)
+
+    if msg.filtered_input_str:
+        qu = msg.filtered_input_str
+        await msg.edit(f"`Seraching Wallpapers for {qu}`")
+        results = requests.get(
+            "https://api.unsplash.com/search/"
+            f"photos?client_id=HWlOs9dNZIbYEkjp87fiEzC9rmE6rKM64tBqXBOLzu8&query={qu}"
+        )
+
+        if results.status_code != 200:
+            return await msg.edit('**Result Not Found**')
+        _json = results.json()['results']
+        if len(_json) < limit:
+            limit = len(_json)
+
+        ss = []
+        os.mkdir("wallpapers")
+
+        for i in range(limit):
+            img = f"wallpapers/wall_{i+1}.png"
+
+            if '-doc' in msg.flags:
+                await pool.run_in_thread(wget.download)(_json[i]['urls']['raw'], img)
+                ss.append(InputMediaDocument(str(img)))
+                continue
+
+            await pool.run_in_thread(wget.download)(_json[i]['urls']['thumb'], img)
+            image = Image.open(img)
+            if not (image.height <= 1280 and image.width <= 1280):
+                image.thumbnail((1280, 1280), Image.ANTIALIAS)
+                a_dex = image.mode.find("A")
+                if a_dex != -1:
+                    new_im = Image.new('RGB', image.size, (255, 255, 255))
+                    new_im.paste(image, mask=image.split()[a_dex])
+                    new_im.save(img, 'JPEG', optimize=True)
+            ss.append(InputMediaPhoto(str(img)))
+
+        await msg.reply_chat_action(
+            "upload_photo" if '-doc' not in msg.flags else "upload_document")
+        await msg.reply_media_group(ss, True)
+        shutil.rmtree("wallpapers/", ignore_errors=True)
+        await msg.delete()
     else:
-        await message.edit('**Give me Something to search.**')
-        await message.client.send_sticker(cat_id, 'CAADAQADmQADTusQR6fPCVZ3EhDoFgQ')
+        await msg.edit('**Give me Something to search.**')
+        await msg.reply_sticker('CAADAQADmQADTusQR6fPCVZ3EhDoFgQ')
