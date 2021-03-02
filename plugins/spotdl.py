59e1c17a5445318ad3f921717b6e4b0b993154b1
Phyco-Ninja <60768764+Phyco-Ninja@users.noreply.github.com>
2021-03-03 00:16:47 +0530

    Song Modules update (#183) closes #156
    * better
    
    * Update requirements.txt
    
    * good.,,,  album/playlist left
    
    * Update deezloader.py
    
    * Update deezloader.py
    
    * this should do. 
    
    Already matching link with regex so it won't matter. if/else will do the job.

--- plugins/spotdl.py
+++ plugins/spotdl.py
@@ -1,39 +1,42 @@
-import os
-import shutil
-import asyncio
 from pathlib import Path
+from typing import Union
 
-from userge import userge, Message, Config
+from spotdl.search import spotifyClient
+from spotdl.search.songObj import SongObj
+from spotdl.download.downloader import DownloadManager
+
+from userge import userge, Message, pool
 from userge.plugins.misc.upload import audio_upload
-from userge.plugins.tools.executor import Term
-
-TEMP_DIR = "spotdl/"
 
 
-@userge.on_cmd("spotdl", about={
-    'header': "Spotify Downloader",
-    'description': "Download Songs via Spotify Links"
-                   " or just by giving song names. ",
-    'usage': "{tr}spotdl [Spotify Link or Song Name]",
-    'examples': "{tr}spotdl https://open.spotify.com/track/0Cy7wt6IlRfBPHXXjmZbcP"})
-async def spotify_dl(message: Message):
-    if not os.path.exists(TEMP_DIR):
-        os.makedirs(TEMP_DIR)
-    song_or_link = message.input_str
-    await message.edit(f"`Downloading {song_or_link} ...`")
-    cmd = f"cd {TEMP_DIR} && spotdl {song_or_link}"
-    runn = await Term.execute(cmd)
-    while not runn.finished:
-        await asyncio.sleep(1)
-        if runn.read_line and len(runn.read_line) <= Config.MAX_MESSAGE_LENGTH:
-            await message.try_to_edit(f">><code>{runn.read_line}</code>")
-    if len(os.listdir(TEMP_DIR)) <= 1:
-        await message.err("Download Failed.")
-    else:
-        await message.delete()
-        for track in os.listdir(TEMP_DIR):
-            if track.startswith("Temp"):
-                continue
-            track_loc = TEMP_DIR + track
-            await audio_upload(message, Path(track_loc), True)
-    shutil.rmtree(TEMP_DIR, ignore_errors=True)
+def init_client() -> None:
+    try:
+        spotifyClient.initialize(
+            clientId='4fe3fecfe5334023a1472516cc99d805',
+            clientSecret='0f02b7c483c04257984695007a4a8d5c'
+        )
+    except Exception:
+        pass
+
+
+async def download_track(url: Union[str, SongObj]) -> Path:
+    init_client()
+    song = url
+    if not isinstance(url, SongObj):
+        song = await pool.run_in_thread(SongObj.from_url)(song)
+    return await DownloadManager().download_song(song)
+
+
+@userge.on_cmd("stdl", about={
+    'header': "Spotify Track Downloader",
+    'description': "Download Songs via Spotify Links",
+    'usage': "{tr}stdl [Spotify Link]",
+    'examples': "{tr}stdl https://open.spotify.com/track/0Cy7wt6IlRfBPHXXjmZbcP"})
+async def spotify_dl(message: Message) -> None:
+    link = message.input_str
+    await message.edit(f"`Downloading: {link} ...`")
+    try:
+        track = await download_track(link)
+    except Exception as e:
+        return await message.err(str(e))
+    await audio_upload(message, track, True)
