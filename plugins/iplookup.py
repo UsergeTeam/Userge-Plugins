3e2504647d3a0d7857fd37a4f2b906a6678d12ef
Phyco-Ninja <60768764+Phyco-Ninja@users.noreply.github.com>
2021-03-04 22:49:13 +0530

    minor changes
    Fixed all anti patterns
    Name convections etc, replaced requests with aiohttp,

--- plugins/iplookup.py
+++ plugins/iplookup.py
@@ -2,42 +2,51 @@
 # A IP Address Lookup Plugin!
 # Modded from @AHToolsBot by @Discovery_Updates
 
-import requests
-import json
+import aiohttp
+
 from userge import userge, Message
+
 
 @userge.on_cmd(
     "iplook", about={
         'header': "A IPLookUp Plugin",
         'description': "Put IP Address to get some details about that.",
         'usage': "{tr}iplook [IP Address]"})
-
-async def IPLookUpTool(message: Message):
-    await message.edit("`Checking IP Address ...`", parse_mode="Markdown")
+async def _ip_look_up(message: Message):
+    await message.edit("`Checking IP Address ...`")
     if not message.input_str:
         await message.edit("`No IP Address Found!`")
         return
-    ip_address = message.input_str
-    url = "https://extreme-ip-lookup.com/json/"
-    data = requests.get(url+ip_address).content.decode('utf-8')
-    values = json.loads(data)
+    url = f"https://extreme-ip-lookup.com/json/{message.input_str}"
+    async with aiohttp.ClientSession() as requests:
+        data = await requests.get(url)
+        values = await data.json()
     status = values['status']
     if status != "success":
-        await message.edit("`Your IP Address Not Valid!`")
+        await message.edit("`Provided IP Address invalid!`")
         return
-    HOST_name = values['ipName']
-    ISP_value = values['isp']
-    org_value = values['org']
-    continent_value = values['continent']
-    IP_Type = values['ipType']
-    country_value = values['country']
-    region_value = values['region']
-    city_value = values['city']
-    localisation = values['lat']+','+values['lon']
-    gmap_lock = "https://www.google.fr/maps?q="+localisation
+    host = values['ipName']
+    isp = values['isp']
+    org = values['org']
+    continent = values['continent']
+    tip = values['ipType']
+    country = values['country']
+    region = values['region']
+    city = values['city']
+    localisation = f"{values['lat']}, {values['lon']}"
+    gmap_lock = f"https://www.google.fr/maps?q={localisation}"
 
     await message.edit(
-        text=f"Here details of `{ip_address}`\n\n**Host:** `{HOST_name}`\n**ISP:** `{ISP_value}`\n**Organisation:** `{org_value}`\n**Region:** `{region_value}`\n**Continent:** `{continent_value}`\n**IP Type:** `{IP_Type}`\n**City:** `{city_value}`\n**Location:** `{localisation}`\n**Google Map:** {gmap_lock}",
+        text=(f"Here details of `{message.input_str}`\n\n"
+              f"**Host:** `{host}`\n"
+              f"**ISP:** `{isp}`\n"
+              f"**Organisation:** `{org}`\n"
+              f"**Region:** `{region}, {country}`\n"
+              f"**Continent:** `{continent}`\n"
+              f"**IP Type:** `{tip}`\n"
+              f"**City:** `{city}`\n"
+              f"**Location:** `{localisation}`\n"
+              f"**Google Map:** {gmap_lock}"),
         disable_web_page_preview=True,
         parse_mode="Markdown"
     )
