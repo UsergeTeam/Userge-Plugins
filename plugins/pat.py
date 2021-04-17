781341786534eb60a342bd3cc00368b13b45344a
Shahnawaz Alam <55076817+prono69@users.noreply.github.com>
2021-04-17 11:16:43 +0530

    Hm

--- plugins/pat.py
+++ plugins/pat.py
@@ -2,7 +2,6 @@ from os import remove
 from urllib import parse
 from random import choice
 import aiohttp
-
 import requests
 
 from userge import userge, Message
@@ -29,20 +28,20 @@ async def pat(message: Message):
     with open(PAT_IMAGE, 'wb') as f:
         f.write(requests.get(pat).content)
     if "-g" in message.flags:
-    	async with aiohttp.ClientSession() as session:
-    		r = "https://nekos.life/api/pat"
-    		async with session.get(r) as request:
-    			result = await request.json()
-    			link = result.get("url", None)
-    			await message.client.send_animation(
-    			message.chat.id, animation=link, reply_to_message_id=reply_id)
+        async with aiohttp.ClientSession() as session:
+            r = "https://nekos.life/api/pat"
+            async with session.get(r) as request:
+                result = await request.json()
+                link = result.get("url", None)
+                await message.client.send_animation(
+                    message.chat.id, animation=link, reply_to_message_id=reply_id)
     else:
-    	if username:
-    		await message.reply_photo(
-    		photo=PAT_IMAGE, caption=username, reply_to_message_id=message.message_id)
-    	else:
-    		await message.reply_photo(
-    		photo=PAT_IMAGE, reply_to_message_id=message.reply_to_message.message_id)
-    
+        if username:
+            await message.reply_photo(
+                photo=PAT_IMAGE, caption=username, reply_to_message_id=message.message_id)
+        else:
+            await message.reply_photo(
+                photo=PAT_IMAGE, reply_to_message_id=message.reply_to_message.message_id)
+
     await message.delete()  # hmm
     remove(PAT_IMAGE)
