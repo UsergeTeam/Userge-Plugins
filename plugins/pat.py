7bfd406715aadc940b9543933efe769ba3f7ce4f
Phyco-Ninja <60768764+Phyco-Ninja@users.noreply.github.com>
2021-04-17 22:19:30 +0530

    minor Changes

--- plugins/pat.py
+++ plugins/pat.py
@@ -1,13 +1,8 @@
-from os import remove
 from urllib import parse
 from random import choice
 import aiohttp
-import requests
 
 from userge import userge, Message
-
-BASE_URL = "https://headp.at/pats/{}"
-PAT_IMAGE = "pat.jpg"
 
 
 @userge.on_cmd("pat", about={
@@ -15,33 +10,26 @@ PAT_IMAGE = "pat.jpg"
     'flags': {'-g': "For Pat Gifs"},
     'usage': "{tr}pat [reply | username]\n{tr}pat -g [reply]"})
 async def pat(message: Message):
-    username = message.input_str
+    username = message.filtered_input_str
     reply = message.reply_to_message
-    reply_id = reply.message_id if reply else None
+    reply_id = reply.message_id if reply else message.message_id
     if not username and not reply:
         await message.edit("**Bruh** ~`Reply to a message or provide username`", del_in=3)
         return
+    kwargs = {"reply_to_message_id": reply_id, "caption": username}
 
     if "-g" in message.flags:
         async with aiohttp.ClientSession() as session:
-            r = "https://nekos.life/api/pat"
-            async with session.get(r) as request:
+            async with session.get("https://nekos.life/api/pat") as request:
                 result = await request.json()
-                link = result.get("url", None)
+                link = result.get("url")
                 await message.client.send_animation(
-                    message.chat.id, animation=link, reply_to_message_id=reply_id)
+                    message.chat.id, animation=link, **kwargs
+                )
     else:
-        resp = requests.get("http://headp.at/js/pats.json")
-        pats = resp.json()
-        _pat = BASE_URL.format(parse.quote(choice(pats)))
-        with open(PAT_IMAGE, 'wb') as f:
-            f.write(requests.get(_pat).content)
-        if username:
-            await message.reply_photo(
-                photo=PAT_IMAGE, caption=username, reply_to_message_id=message.message_id)
-        else:
-            await message.reply_photo(
-                photo=PAT_IMAGE, reply_to_message_id=message.reply_to_message.message_id)
+        async with aiohttp.ClientSession() as session:
+            chi_c = await session.get("https://headp.at/js/pats.json")
+            uri = f"https://headp.at/pats/{parse.quote(choice(await chi_c.json()))}"
+        await message.reply_photo(uri, **kwargs)
 
     await message.delete()  # hmm
-    remove(PAT_IMAGE)
