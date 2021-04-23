4af5a6251f372dd0287f35c24867580a2a725e35
Jigar varma <71844973+Jigarvarma2005@users.noreply.github.com>
2021-04-23 17:46:21 +0530

    😌
    😌

--- plugins/logo_maker.py
+++ plugins/logo_maker.py
@@ -1,67 +1,92 @@
 # © JigarVarma2005
 # Logo maker using brandcrowd.com
 # Moded from @JVToolsBot by @UniversalBotsUpdate
+
 import os
-import random
 import requests
 from bs4 import BeautifulSoup
 from userge import userge, Message, Config
 
-def download_images(images):
-    count = 0
-    print(f"Total {len(images)} Image Found!")
-    if len(images) != 0:
-        for i, image in enumerate(images):
-            try:
-                image_link = image["data-srcset"]
-            except:
-                try:
-                    image_link = image["data-src"]
-                except:
-                    try:
-                        image_link = image["data-fallback-src"]
-                    except:
-                        try:
-                            image_link = image["src"]
-                        except:
-                            pass
-            try:
-                r = requests.get(image_link).content
-                try:
-                    r = str(r, "utf-8")
-                except UnicodeDecodeError:
-                    with open(f"{Config.DOWN_PATH}/logo.jpg", "wb+") as f:
-                        f.write(r)
-                    count += 1
-            except:
-                pass
+
+@userge.on_cmd("logo", about={
+    'header': "Get a logo from brandcrowd",
+    'usage': "{tr}logo text:keyword"})
+async def jv_logo_maker(message: Message):
+
+    jv_text = message.input_str
+    await message.edit("Please wait...")
+
+    if ':' not in jv_text:
+
+        type_keyword = "name"
+        type_text = jv_text
+
+    else:
+
+        jv = jv_text.split(":", 1)
+        type_keyword = jv[1]
+        type_text = jv[0]
+
+    images = main_logo(type_text, type_keyword)
+    image_list = download_images(images)
+
+    if not image_list:
+        return await message.err("Images not Found!")
+
+    for i in image_list:
+        if os.path.exists(i):
+            await message.client.send_photo(message.chat.id, i)
+            os.remove(i)
+    await message.delete()
+
 
 def main_logo(type_text, type_keyword):
+
     url = f"https://www.brandcrowd.com/maker/logos?text={type_text}&searchtext={type_keyword}&searchService="
     r = requests.get(url)
     soup = BeautifulSoup(r.text, "html.parser")
     images = soup.findAll("img")
-    random.shuffle(images)
-    download_images(images)
-    
-@userge.on_cmd("logo", about={
-  'header': "Get a logo from brandcrowd",
-  'usage': "{tr}logo text:keyword"})
-async def jv_logo_maker(message: Message):
-    jv_text = message.input_str
-    await message.edit("Please wait...")
-    if ':' not in jv_text:
-        type_keyword = "name"
-        type_text = jv_text
-    else:
-        jv = jv_text.split(":", 1)
-        type_keyword = jv[1]
-        type_text = jv[0]
-    main_logo(type_text, type_keyword)
-    logo_path = f"{Config.DOWN_PATH}/logo.jpg"
-    await message.client.send_photo(message.chat.id, logo_path)
-    await message.delete()
-    try:
-        os.remove(logo_path)
-    except:
-        pass
+    return images
+
+
+def download_images(images):
+
+    image_link = None
+    return_list = []
+
+    if len(images) > 0:
+
+        for i, image in enumerate(images):
+
+            if image.get("data-srcset"):
+                image_link = image.get("data-srcset")
+
+            elif image.get("data-src"):
+                image_link = image["data-src"]
+
+            elif image.get("data-fallback-src"):
+                image_link = image.get("data-fallback-src")
+
+            elif image.get("src"):
+                image_link = image.get("src")
+
+            if not image_link:
+                return None
+
+            try:
+                r = requests.get(image_link).content
+
+                try:
+                    r = str(r, "utf-8")
+                except UnicodeDecodeError:
+                    with open(f"{Config.DOWN_PATH}/logo_{i}.jpg", "wb+") as f:
+                        f.write(r)
+
+                return_list.append(f"{Config.DOWN_PATH}/logo_{i}.jpg")
+
+            except Exception:
+                pass
+
+        return return_list
+
+    return None
