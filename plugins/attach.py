126524e16f574464983ccd5dc296524d8dd3fa66
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-06-29 22:39:44 +0530

    Create attach.py

---
+++ plugins/attach.py
@@ -0,0 +1,12 @@
+# Author: Fayas (https://github.com/FayasNoushad) (@FayasNoushad)
+
+from userge import userge, Message
+
+@userge.on_cmd("attach", about={
+    'header': "Attach any link in a message",
+    'usage': "{tr}attach [reply to a message]"})
+async def attach(message:Message):
+    '''Attach links in message'''
+    link = message.text.split(" ", -1)
+    text = message.reply_to_message.text
+    await message.reply_text(text=f"[\u2063]({link}){text}")
