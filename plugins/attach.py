4334e836ef73e488180818633b96206b5a5648de
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-06-29 22:45:19 +0530

    Update attach.py

--- plugins/attach.py
+++ plugins/attach.py
@@ -5,7 +5,7 @@ from userge import userge, Message
 @userge.on_cmd("attach", about={
     'header': "Attach any link in a message",
     'usage': "{tr}attach [reply to a message]"})
-async def attach(message:Message):
+async def attach(message: Message):
     '''Attach links in message'''
     link = message.text.split(" ", -1)
     text = message.reply_to_message.text
