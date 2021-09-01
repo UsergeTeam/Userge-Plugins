1decc46982f92b23bc22f3fb083018269d2ef46e
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-09-01 22:10:15 +0530

    Update attach.py

--- plugins/attach.py
+++ plugins/attach.py
@@ -4,16 +4,16 @@ from userge import userge, Message
 
 
 @userge.on_cmd("attach", about={
-    'header': "Attach any link in a message",
-    'usage': "{tr}attach [reply to a message]"})
+    'header': "Attach any link's preview in a message",
+    'usage': "{tr}attach [link] [reply to a message]"})
 async def attach(update: Message):
-    '''Attach links in message''' 
-    if update.reply_to_message is None:
+    '''Attach links in message'''
+    link = update.input_str
+    replied = update.reply_to_message
+    if replied is None and not link:
         await update.reply_text(
-            text="Reply to a text for attaching"
+            text="`Reply to a text for attachment and provide link as input...`"
         )
         return
-    message = update.reply_to_message
-    link = message.text.split(" ", 1)[1]
-    text = message.text
-    await message.reply_text(text=f"[\u2063]({link}){text}")
+    text = replied.text
+    await replied.reply_text(text=f"[\u2063]({link}){text}")
