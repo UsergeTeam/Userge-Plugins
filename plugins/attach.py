70af643df6d101b0a8a0be5441fae38387ea1a90
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-09-02 06:49:08 +0530

    Update attach.py

--- plugins/attach.py
+++ plugins/attach.py
@@ -8,9 +8,9 @@ from userge import userge, Message
     'usage': "{tr}attach [link] [reply to a message]"})
 async def attach(update: Message):
     '''Attach links in message'''
-    link = update.input_str
+    link = update.text.split()[1]
     replied = update.reply_to_message
-    if replied is None and not link:
+    if replied is None or not link:
         await update.reply_text(
             text="`Reply to a text for attachment and provide link as input...`"
         )
