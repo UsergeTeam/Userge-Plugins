8abd6983345e18e8b662a8914981eb92c2722a25
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-09-02 19:36:57 +0530

    Added attach links in a message support (#208)
    * Create attach.py
    
    * Update attach.py
    
    * Update attach.py
    
    * Update attach.py
    
    * Update attach.py
    
    * Update attach.py
    
    * Update attach.py
    
    * Update attach.py
    
    * Update attach.py
    
    * Update attach.py

---
+++ plugins/attach.py
@@ -0,0 +1,20 @@
+# Author: Fayas (https://github.com/FayasNoushad) (@FayasNoushad)
+
+from userge import userge, Message
+
+
+@userge.on_cmd("attach", about={
+    'header': "Attach any link's preview in a message",
+    'usage': "{tr}attach [link] [reply to a message]"})
+async def attach(update: Message):
+    '''Attach links in message'''
+    link = update.text.split()[1]
+    replied = update.reply_to_message
+    if replied is None or not link:
+        await update.reply_text(
+            text="`Reply to a text for attachment and provide link as input...`"
+        )
+        return
+    text = replied.text
+    await replied.edit_text(text=f"[\u2063]({link}){text}")
+    await update.delete()
