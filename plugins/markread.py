690ed6a149b827b39c454c887846e08c55887bc4
Krishna-singhal <65902764+Krishna-Singhal@users.noreply.github.com>
2022-01-05 00:34:00 +0530

    Create markread.py

---
+++ plugins/markread.py
@@ -0,0 +1,28 @@
+# Plugin by @Krishna_Singhal
+
+from pyrogram.raw import functions
+from pyrogram.errors import PeerIdInvalid
+
+from userge import userge, Message
+
+
+@userge.on_cmd("markread", about={
+    'header': "Mark read all chats.",
+    'usage': "{tr}markread"},
+    allow_via_bot=False, allow_channels=False
+)
+async def mark_read(msg: Message):
+    await msg.edit("Processing...")
+    total = 0
+    failed = 0
+
+    chats = await userge.send(
+        functions.messages.GetAllChats(except_ids=[])
+    )
+    for i in chats:
+        total += 1
+        try:
+            await userge.read_history(chat_id=i.id)
+        except PeerIdInvalid:
+            failed += 1
+    await msg.edit(f"Marked {total - failed} chats as read from {total}")
