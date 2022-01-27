ab08dc0366ac539ff5ec1f4625308412eb6b24be
Shrimadhav U K <SpEcHiDe@users.noreply.github.com>
2022-01-28 02:42:26 +0530

    `IMDB:` add inline support  (#244)

--- plugins/imdb.py
+++ plugins/imdb.py
@@ -1,7 +1,15 @@
 import json
 import os
 import requests
-
+from pyrogram import filters
+from pyrogram.types import (
+    CallbackQuery,
+    InlineQuery,
+    InlineQueryResultArticle,
+    InputTextMessageContent,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup
+)
 from userge import userge, Message, Config, pool
 
 THUMB_PATH = Config.DOWN_PATH + "imdb_thumb.jpg"
@@ -16,7 +24,8 @@ API_TWO_URL = os.environ.get("IMDB_API_TWO_URL")
     'description': "Get info about a Movie on IMDB.\n"
                    "[NOTE: To use a custom poster, download "
                    "the poster with name imdb_thumb.jpg]",
-    'usage': "{tr}imdb [Movie Name]"})
+    'usage': "{tr}imdb [Movie Name]",
+    'use inline': "@botusername imdb [Movie Name]"})
 async def imdb(message: Message):
     if not (API_ONE_URL or API_TWO_URL):
         return await message.err(
@@ -26,23 +35,63 @@ async def imdb(message: Message):
     try:
         movie_name = message.input_str
         await message.edit(f"__searching IMDB for__ : `{movie_name}`")
-        search_results = await _get(API_ONE_URL.format(theuserge=movie_name))
-        srch_results = json.loads(search_results.text)
-        first_movie = srch_results.get("d")[0]
-        mov_title = first_movie.get("l")
-        mov_imdb_id = first_movie.get("id")
-        mov_link = f"https://www.imdb.com/title/{mov_imdb_id}"
-        page2 = await _get(API_TWO_URL.format(imdbttid=mov_imdb_id))
-        second_page_response = json.loads(page2.text)
-        image_link = first_movie.get("i").get("imageUrl")
-        mov_details = get_movie_details(second_page_response)
-        director, writer, stars = get_credits_text(second_page_response)
-        story_line = second_page_response.get("summary").get("plot", 'Not available')
-        mov_country, mov_language = get_countries_and_languages(second_page_response)
-        mov_rating = second_page_response.get("UserRating").get("description")
-        des_ = f"""<b>Title🎬: </b><code>{mov_title}</code>
+        response = await _get(API_ONE_URL.format(theuserge=movie_name))
+        srch_results = json.loads(response.text)
+        mov_imdb_id = srch_results.get("d")[0].get("id")
+        image_link, description = await get_movie_description(mov_imdb_id)
+    except (IndexError, json.JSONDecodeError, AttributeError):
+        await message.edit("Bruh, Plox enter **Valid movie name** kthx")
+        return
 
-<b>More Info: </b><code>{mov_details}</code>
+    if os.path.exists(THUMB_PATH):
+        await message.client.send_photo(
+            chat_id=message.chat.id,
+            photo=THUMB_PATH,
+            caption=description,
+            parse_mode="html"
+        )
+        await message.delete()
+    elif image_link is not None:
+        await message.client.send_photo(
+            chat_id=message.chat.id,
+            photo=image_link.replace("_V1_", "_V1_UX720"),
+            caption=description,
+            parse_mode="html"
+        )
+        await message.delete()
+    else:
+        await message.edit(
+            description,
+            disable_web_page_preview=True,
+            parse_mode="HTML"
+        )
+
+
+async def get_movie_description(imdb_id):
+    response = await _get(API_TWO_URL.format(imdbttid=imdb_id))
+    soup = json.loads(response.text)
+
+    mov_link = f"https://www.imdb.com/title/{imdb_id}"
+    image_link = soup.get('poster')
+    genres = soup.get("genres")
+    duration = soup.get("duration")
+    mov_rating = soup.get("UserRating").get("rating")
+    if mov_rating.strip() == '/':
+        mov_rating = "`Ratings not found!`"
+    else:
+        users = soup.get("UserRating").get("numeric_description_only")
+        if users:
+            mov_rating += f" (based on {users} users)"
+    if duration:
+        genres.append(duration)
+
+    mov_country, mov_language = get_countries_and_languages(soup)
+    director, writer, stars = get_credits_text(soup)
+    story_line = soup.get("summary").get("plot", 'Not available')
+
+    description = f"<b>Title</b><a href='{image_link}'>🎬</a>: <code>{soup.get('title')}</code>"
+    description += f"""
+<b>Genres: </b><code>{' '.join(genres) if len(genres) > 0 else ''}</code>
 <b>Rating⭐: </b><code>{mov_rating}</code>
 <b>Country🗺: </b><code>{mov_country}</code>
 <b>Language: </b><code>{mov_language}</code>
@@ -54,53 +103,10 @@ async def imdb(message: Message):
 <b>IMDB URL Link🔗: </b>{mov_link}
 
 <b>Story Line : </b><em>{story_line}</em>"""
-    except IndexError:
-        await message.edit("Bruh, Plox enter **Valid movie name** kthx")
-        return
-    if len(des_) > 1024:
-        des_ = des_[:1021] + "..."
-    if os.path.exists(THUMB_PATH):
-        optimize_image(THUMB_PATH)
-        await message.client.send_photo(
-            chat_id=message.chat.id,
-            photo=THUMB_PATH,
-            caption=des_,
-            parse_mode="html"
-        )
-        await message.delete()
-    elif image_link is not None:
-        await message.edit("__downloading thumb ...__")
-        op_img_link = optimize_image(image_link)
-        await message.client.send_photo(
-            chat_id=message.chat.id,
-            photo=op_img_link,
-            caption=des_,
-            parse_mode="html"
-        )
-        await message.delete()
-        os.remove(op_img_link)
-    else:
-        await message.edit(des_, parse_mode="HTML")
 
-
-def optimize_image(image_url):
-    return image_url.replace("_V1_", "_V1_UX360")
-
-
-def get_movie_details(soup):
-    mov_details = []
-    inline = soup.get("genres")
-    if inline and len(inline) > 0:
-        for io in inline:
-            mov_details.append(io)
-    tags = soup.get("duration")
-    if tags:
-        mov_details.append(tags)
-    if mov_details and len(mov_details) > 1:
-        mov_details_text = ' | '.join(mov_details)
-    else:
-        mov_details_text = mov_details[0] if mov_details else ''
-    return mov_details_text
+    if len(description) > 1024:
+        description = description[:1021] + "..."
+    return image_link, description
 
 
 def get_countries_and_languages(soup):
@@ -163,3 +169,89 @@ def _get(url: str, attempts: int = 0) -> requests.Response:
             break
         attempts += 1
     return abc
+
+
+if userge.has_bot:
+
+    @userge.bot.on_callback_query(filters=filters.regex(pattern=r"imdb\((.+)\)"))
+    async def imdb_callback(_, c_q: CallbackQuery):
+        if c_q.from_user.id == Config.OWNER_ID[0]:
+            imdb_id = str(c_q.matches[0].group(1))
+            _, description = await get_movie_description(imdb_id)
+            await c_q.edit_message_text(
+                text=description,
+                disable_web_page_preview=False,
+                reply_markup=InlineKeyboardMarkup(
+                    [
+                        [
+                            InlineKeyboardButton(
+                                text="Open IMDB!",
+                                url=f"https://imdb.com/title/{imdb_id}"
+                            )
+                        ]
+                    ]
+                )
+            )
+        else:
+            await c_q.answer("This is not for you", show_alert=True)
+
+    @userge.bot.on_inline_query(
+        filters.create(
+            lambda _, __, inline_query: (
+                inline_query.query
+                and inline_query.query.startswith("imdb ")
+            ),
+            # https://t.me/UserGeSpam/359404
+            name="ImdbInlineFilter"
+        ),
+        group=-1
+    )
+    async def inline_fn(_, inline_query: InlineQuery):
+        movie_name = inline_query.query.split("imdb ")[1].strip()
+        search_results = await _get(API_ONE_URL.format(theuserge=movie_name))
+        srch_results = json.loads(search_results.text)
+        asroe = srch_results.get("d")
+        oorse = []
+        for sraeo in asroe:
+            title = sraeo.get("l", "")
+            description = sraeo.get("q", "")
+            stars = sraeo.get("s", "")
+            imdb_url = f"https://imdb.com/title/{sraeo.get('id')}"
+            year = sraeo.get("yr", "").rstrip('-')
+            image_url = sraeo.get("i").get("imageUrl")
+            message_text = f"<a href='{image_url}'>🎬</a>"
+            message_text += f"<a href='{imdb_url}'>{title} {year}</a>"
+            oorse.append(
+                InlineQueryResultArticle(
+                    title=f" {title} {year}",
+                    input_message_content=InputTextMessageContent(
+                        message_text=message_text,
+                        parse_mode="html",
+                        disable_web_page_preview=False
+                    ),
+                    url=imdb_url,
+                    description=f" {description} | {stars}",
+                    thumb_url=image_url,
+                    reply_markup=InlineKeyboardMarkup(
+                        [
+                            [
+                                InlineKeyboardButton(
+                                    text="Get IMDB details",
+                                    callback_data=f"imdb({sraeo.get('id')})"
+                                )
+                            ]
+                        ]
+                    )
+                )
+            )
+        resfo = srch_results.get("q")
+        await inline_query.answer(
+            results=oorse,
+            cache_time=300,
+            is_gallery=False,
+            is_personal=False,
+            next_offset="",
+            switch_pm_text=f"Found {len(oorse)} results for {resfo}",
+            switch_pm_parameter="imdb"
+        )
+        inline_query.stop_propagation()
