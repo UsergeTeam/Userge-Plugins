6d07d0b369cce6fff38466b64c8b0f02fffda3c9
Krishna-singhal <65902764+Krishna-Singhal@users.noreply.github.com>
2022-02-01 18:32:15 +0530

    Update create.py

--- plugins/create.py
+++ plugins/create.py
@@ -6,7 +6,10 @@ from userge import userge, Message
 
 @userge.on_cmd(
     "channel",
-    about="Creates a channel",
+    about={
+        'header': "Creates a channel",
+        'usage': "{tr}channel TheUserge | Hello"
+    },
     allow_channels=False,
     allow_bots=False
 )
@@ -15,7 +18,14 @@ async def create_channel(message: Message):
         args = message.input_str
         if not args:
             return await message.err("title not found!")
-        await userge.create_channel(args, 'nice')
-        await message.edit(f"Successfully made a new channel **{args}**")
+
+        if '|' in args:
+            title, des = args.split('|', maxsplit=1)
+        else:
+            title, des = args, "This channel is created using @TheUserge"
+        if len(des) > 256:
+            des = des.strip()[:253] + "..."
+        await userge.create_channel(title.strip(), des.strip())
+        await message.edit(f"Successfully made a new channel **{title.strip()}**")
     except Exception as e:
         await message.err(str(e))
