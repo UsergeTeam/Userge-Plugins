489a0fa34f1bb2ed1882f226710aca45dc9e901d
Shrimadhav U K <SpEcHiDe@users.noreply.github.com>
2022-03-05 10:01:15 +0530

    attempt 5: try to add env vars

--- plugins/utils/imdb/__init__.py
+++ plugins/utils/imdb/__init__.py
@@ -12,3 +12,5 @@ import os
 
 API_ONE_URL = os.environ.get("IMDB_API_ONE_URL")
 API_TWO_URL = os.environ.get("IMDB_API_TWO_URL")
+API_THREE_URL = os.environ.get("API_THREE_URL")
+WATCH_COUNTRY = os.environ.get("WATCH_COUNTRY", "en_IN")
