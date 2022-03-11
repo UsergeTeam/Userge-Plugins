9775b44258d74bff0dbb7c3f97e54c9105fc0b63
jaxellis <67706810+jaxellis@users.noreply.github.com>
2022-03-11 00:02:14 -0800

    Time: Update
    * New flag -code | -c to use a country code manually
    * Switch info logs to debug.
    * New output style.
    * Update original -l flag with -list
    * Proper error catching. Should never fail to output.
    * Switch list link to hosting on github.
    No need to worry about third party providers keeping list.
    
    Old:
    It is currently 11:52 PM on 10-03-2022 in America/Los Angeles
    
    New:
    It's 11:52PM on Thursday March 10th in America/Los Angeles.

--- plugins/utils/time/__main__.py
+++ plugins/utils/time/__main__.py
@@ -8,7 +8,7 @@
 #
 # All rights reserved.
 
-from datetime import datetime as dt
+from datetime import datetime
 
 from pytz import timezone
 
@@ -21,35 +21,77 @@ LOG = userge.getLogger(__name__)  # logger object
 @userge.on_cmd("dt", about={
     'header': "Get the time and date of a City/Country/Timezone.",
     'flags': {
-        '-l': "Gives list of all Country/City Combos for Heroku Config"},
-    'usage': "Use {tr}dt to show the Time & Date of your predefined City\n"
-             "Use {tr}dt -l to display all TZ Combo's for the Config\n",
-    'examples': ['{tr}dt', '{tr}dt [Flag]']},
-    del_pre=True)
-async def grabTime(message: Message):
-    LOG.info("Starting Time command...")
-    defaultMessage = (
+        '-list | -l': "Gives a list of all Country/City Combos.",
+        '-code | -c': "Uses Country_City code given."
+    },
+    'usage':
+        "{tr}dt to show the Time & Date of your predefined City.\n"
+        "{tr}dt -list | {tr}dt -l to display all TZ Combo's for the Config.\n"
+        "{tr}dt -code | {tr}dt -c to use a defined Country/City combo.",
+
+    'examples': ['{tr}dt', '{tr}dt [Flag]']
+}, del_pre=True)
+async def grab_time(message: Message):
+    LOG.debug("Time: Starting now...")
+    country_input = await flag_checks(message, LOG)
+    if country_input is None:
+        return
+    country_code = COUNTRY_CITY if not country_input else country_input
+    try:
+        timezone(country_code)
+    except BaseException:
+        LOG.debug("Time: Incorrect Country Code...")
+        await message.err("Unable To Determine Timezone With Given"
+                          + " Country Code | " + country_code +
+                          "\nUse the -l flag to see compatible codes.")
+        return
+    datetime_now = datetime.now(timezone(country_code))
+    date_day = datetime_now.strftime("%d")
+    date_time = datetime_now.strftime('%I:%M%p')
+    if date_day[0] == "0":
+        date_day = date_day[1:]
+    if date_time[0] == "0":
+        date_time = date_time[1:]
+    await message.edit(" ".join(
+        ["It's", date_time, "on",
+         datetime_now.strftime('%A'), datetime_now.strftime('%B'),
+         date_day + ordinal_suffix(int(date_day)), "in",
+         country_code.replace("_", " ") + "."
+         ]
+    ))
+    LOG.debug("Time: Command Finished Successfully")
+
+
+def ordinal_suffix(day: int):
+    if 3 < day < 21 or 23 < day < 31:
+        return 'th'
+    else:
+        return {1: 'st', 2: 'nd', 3: 'rd'}[day % 10]
+
+
+async def flag_checks(message: Message, log):
+    default_message = (
         "<code>Below is a list of all the Timezones Avaliable</code> \n<a "
-        "href=https://pastebin.com/raw/0KSh9CMj>Click Here!</a>\n<code>Enter"
-        " one in your Heroku Config Under</code> (<code>COUNTRY_CITY</code>)\n"
+        "href=https://raw.githubusercontent.com/UsergeTeam/Userge-Plugins/main"
+        "/plugins/utils/time/citylist.txt>Click Here!</a>\n<code>Enter one in"
+        " your Config Under</code> (<code>COUNTRY_CITY</code>)\n"
         "<code>Ex: America/Los_Angeles</code>")
-
-    if 'l' in message.flags:
-        LOG.info("Time: List Flag Used: Giving TZ list...")
-        await message.edit(defaultMessage, disable_web_page_preview=True,
+    if 'list' in message.flags or 'l' in message.flags:
+        log.debug("Time | FLAG = List: Giving TZ list...")
+        await message.edit(default_message, disable_web_page_preview=True,
                            parse_mode="html", del_in=30)
-        return
+        return None
 
-    if not COUNTRY_CITY:
-        LOG.info("Time: No Config Set")
-        await message.edit(defaultMessage, disable_web_page_preview=True,
+    if 'code' in message.flags or 'c' in message.flags:
+        log.debug("Time | FLAG = Code: Grabbing Country_Code...")
+        country_input = message.filtered_input_str.strip()
+        if not country_input:
+            await message.err("No Country_City code found after the flag...")
+            return None
+        return country_input
+    elif not COUNTRY_CITY:
+        log.debug("Time: No Config Set")
+        await message.edit(default_message, disable_web_page_preview=True,
                            parse_mode="html", del_in=30)
-        return
-
-    tzDateTime = dt.now(timezone(COUNTRY_CITY))
-    date = tzDateTime.strftime('%d-%m-%Y')
-    militaryTime = tzDateTime.strftime('%H:%M')
-    time = dt.strptime(militaryTime, "%H:%M").strftime("%I:%M %p")
-    await message.edit("It is currently " + time + " on " + date + " in " +
-                       COUNTRY_CITY.replace("_", " "))
-    LOG.info("Time: Command Finished Successfully")
+        return None
+    return False
