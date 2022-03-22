4daabfc6d6f0b758324875f4b744f65799c62388
SAF ONE <77989182+AsmSafone@users.noreply.github.com>
2022-03-22 23:39:34 +0600

    Added `autofastly` plugin (#279)
    * Added `autofastly` plugin
    
    * fix deepsource
    
    * in some cases
    
    * Update __main__.py
    
    Co-authored-by: Krishna-singhal <65902764+Krishna-Singhal@users.noreply.github.com>

--- plugins/utils/ocr/__init__.py
+++ plugins/utils/ocr/__init__.py
@@ -8,6 +8,41 @@
 
 """ ocr reader """
 
+import requests
+
+from userge import pool
 from userge.utils import secured_env
 
 OCR_SPACE_API_KEY = secured_env("OCR_SPACE_API_KEY")
+
+
+@pool.run_in_thread
+def ocr_space_file(filename,
+                   language='eng',
+                   overlay=False,
+                   api_key=OCR_SPACE_API_KEY):
+    """
+    OCR.space API request with local file.
+        Python3.5 - not tested on 2.7
+    :param filename: Your file path & name.
+    :param overlay: Is OCR.space overlay required in your response.
+                    Defaults to False.
+    :param api_key: OCR.space API key.
+                    Defaults to 'helloworld'.
+    :param language: Language code to be used in OCR.
+                    List of available language codes can be found on https://ocr.space/OCRAPI
+                    Defaults to 'en'.
+    :return: Result in JSON format.
+    """
+    payload = {
+        'isOverlayRequired': overlay,
+        'apikey': api_key,
+        'language': language,
+    }
+    with open(filename, 'rb') as f:
+        r = requests.post(
+            'https://api.ocr.space/parse/image',
+            files={filename: f},
+            data=payload,
+        )
+    return r.json()
