fa7ac87cf8c871e4d68e4b209385191a96f5bc44
Shrimadhav U K <SpEcHiDe@users.noreply.github.com>
2022-03-26 18:04:00 +0530

    eleven

--- plugins/tools/secret_msg/__main__.py
+++ plugins/tools/secret_msg/__main__.py
@@ -115,20 +115,18 @@ async def recv_s_m_o(msg: Message):
             str(msg.message_id)
         ] = {
             "file_id": media_ifdd.file_id,
-            "caption": (
-                msg.reply_to_message.caption and
-                msg.reply_to_message.caption.html
-            ) or ""
+            "caption": (msg.reply_to_message.caption and
+                        msg.reply_to_message.caption.html
+                       ) or ""
         }
     else:
         MEDIA_FID_S[
             str(msg.message_id)
         ] = {
             "file_id": "0",
-            "caption": (
-                msg.reply_to_message.text and
-                msg.reply_to_message.text.html
-            ) or ""
+            "caption": (msg.reply_to_message.text and
+                        msg.reply_to_message.text.html
+                       ) or ""
         }
     await msg.reply_text("click here", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(
         text="send something",
