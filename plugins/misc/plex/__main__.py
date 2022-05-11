b9eb92421db29e0141703766d625e047c68263ef
Fatih Ka <xybydy@gmail.com>
2022-05-11 12:51:11 +0300

    plex devam

--- plugins/misc/plex/__main__.py
+++ plugins/misc/plex/__main__.py
@@ -92,6 +92,7 @@ async def plogin(message: Message):
                 username = trimmed_uname
                 password = trimmed_passwd 
             try:
+                _LOG.debug(Opts.username,Opts.password)
                 account = utils.getMyPlexAccount(Opts)
             except BadRequest as e:
                 await message.edit("Plex login failed. Please check logs.")
@@ -99,7 +100,7 @@ async def plogin(message: Message):
             else:
                 await asyncio.gather(
                 _set_creds(account),
-                message._message.edit("`Saved Plex Creds!`", del_in=3, log=__name__))
+                message.message.edit("`Saved Plex Creds!`", del_in=3, log=__name__))
 
 # u = User(session=account._session, token=account.authenticationToken)
 
