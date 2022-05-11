d5643988b60ee213a2c28e0ccad6109d645a7ab9
Fatih Ka <xybydy@gmail.com>
2022-05-11 13:44:05 +0300

    le

--- plugins/misc/plex/__main__.py
+++ plugins/misc/plex/__main__.py
@@ -115,7 +115,7 @@ async def pservers(message: Message):
     if message.input_str:
         pass
 
-    servers = [s for s in account.resources() if 'server' in s.provides]
+    servers = [s for s in _CREDS.resources() if 'server' in s.provides]
     msg = ""
     for i in range(len(servers)):
         msg+=f"{i}. {servers[i]}\n"
