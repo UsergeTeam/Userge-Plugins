fb727653bd98574e80c456ce125329e4be30db93
1337x <18002848@vcconnect.co.za>
2022-05-11 23:49:04 +0200

    Split text to multiline

--- plugins/utils/domain/__main__.py
+++ plugins/utils/domain/__main__.py
@@ -26,7 +26,9 @@ from userge import userge, Message
     allow_via_bot=False
 )
 async def creator(m: Message) -> None:
-    """ View Your Domain\nAvailable Flags:\n-c: List all groups & channels created by you.\n-a: List all groups and channels administered by you"""
+    "View Your Domain\nAvailable Flags:\n"\
+    "-c: List all groups & channels created by you."\
+    "-a: List all groups and channels administered by you"
 
     await m.edit("__This may take a while, please wait ...__")
 
