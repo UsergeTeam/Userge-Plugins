7b63dc02ac747ba2a284145d0a7a6030d596a7ef
rking32 <sljohnwick@gmail.com>
2022-06-25 22:49:29 +0530

    fix purge command, max msg limit to 1000 and default to 10

--- plugins/admin/purge/__main__.py
+++ plugins/admin/purge/__main__.py
@@ -17,59 +17,61 @@ from userge import userge, Message
     'header': "purge messages from user",
     'flags': {
         '-u': "get user_id from replied message",
-        '-l': "message limit : max 100"},
+        '-l': "message limit : max 1000, def 10"},
     'usage': "reply {tr}purge to the start message to purge.\n"
              "use {tr}purge [user_id | user_name] to purge messages from that user or use flags",
-    'examples': ['{tr}purge', '{tr}purge -u', '{tr}purge [user_id | user_name]']},
-    allow_bots=False, del_pre=True)
+    'examples': ['{tr}purge', '{tr}purge -u', '{tr}purge [user_id | user_name]']}, del_pre=True)
 async def purge_(message: Message):
     await message.edit("`purging ...`")
+
+    limit = min(1000, int(message.flags.get('l') or 10))
     from_user_id = None
-    if message.filtered_input_str:
-        from_user_id = (await message.client.get_users(message.filtered_input_str)).id
-    start_message = 0
-    if 'l' in message.flags:
-        limit = min(100, int(message.flags['l']))
-        start_message = message.id - limit
+
     if message.reply_to_message:
         start_message = message.reply_to_message_id
         if 'u' in message.flags:
             from_user_id = message.reply_to_message.from_user.id
-    if not start_message:
-        await message.err("invalid start message!")
-        return
-    list_of_messages = []
+    else:
+        start_message = message.id - limit
+
+    if not from_user_id and message.filtered_input_str:
+        from_user_id = (await message.client.get_users(message.filtered_input_str)).id
+
     purged_messages_count = 0
+    list_of_messages = []
+
+    async def delete_msgs():
+        nonlocal purged_messages_count
+        await message.client.delete_messages(
+            chat_id=message.chat.id,
+            message_ids=list_of_messages
+        )
+        purged_messages_count += len(list_of_messages)
+        list_of_messages.clear()
 
     async def handle_msg(_msg):
-        nonlocal list_of_messages, purged_messages_count
-        if (from_user_id and _msg and _msg.from_user
-                and _msg.from_user.id == from_user_id):
-            list_of_messages.append(_msg.id)
-        if not from_user_id:
-            list_of_messages.append(_msg.id)
+        if from_user_id and _msg.from_user and _msg.from_user.id != from_user_id:
+            return
+        list_of_messages.append(_msg.id)
         if len(list_of_messages) >= 100:
-            await message.client.delete_messages(
-                chat_id=message.chat.id,
-                message_ids=list_of_messages
-            )
-            purged_messages_count += len(list_of_messages)
-            list_of_messages = []
+            await delete_msgs()
 
     start_t = datetime.now()
+    stop_message = start_message + limit
+
     if message.client.is_bot:
         for msg in await message.client.get_messages(
                 chat_id=message.chat.id, replies=0,
-                message_ids=range(start_message, message.id)):
+                message_ids=range(start_message, stop_message)):
             await handle_msg(msg)
     else:
         async for msg in message.client.get_chat_history(
-                chat_id=message.chat.id, offset_id=start_message):
+                chat_id=message.chat.id, limit=limit, offset_id=stop_message):
             await handle_msg(msg)
+
     if list_of_messages:
-        await message.client.delete_messages(chat_id=message.chat.id,
-                                             message_ids=list_of_messages)
-        purged_messages_count += len(list_of_messages)
+        await delete_msgs()
+
     end_t = datetime.now()
     time_taken_s = (end_t - start_t).seconds
     out = f"<u>purged</u> {purged_messages_count} messages in {time_taken_s} seconds."
