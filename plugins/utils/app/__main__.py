73fc4418239796c3a7b57e55935cac413696456c
rking32 <sljohnwick@gmail.com>
2022-07-07 00:50:52 +0530

    fix app command

--- plugins/utils/app/__main__.py
+++ plugins/utils/app/__main__.py
@@ -30,34 +30,32 @@ async def app(message: Message):
         await message.edit("`Searching...`")
         app_name = "+".join(message.input_str.split(" "))
         async with aiohttp.ClientSession() as ses, ses.get(
-            f"https://play.google.com/store/search?q={app_name}&c=apps"
-        ) as res:
-            result = bs4.BeautifulSoup(
-                await res.text(),
-                "lxml",
-                parse_only=bs4.SoupStrainer("div", class_="ipRz4"),
-            )
+                f"https://play.google.com/store/search?q={app_name}&c=apps") as res:
+            result = bs4.BeautifulSoup(await res.text(), "lxml")
 
-        app_name = result.find("div", class_="vWM94c").text
-        app_dev = result.find("div", class_="LbQbAe").text
+        found = result.find("div", class_="vWM94c")
+        if found:
+            app_name = found.text
+            app_dev = result.find("div", class_="LbQbAe").text
+            app_rating = result.find("div", class_="TT9eCd").text.replace("star", "")
+            _app_link = result.find("a", class_="Qfxief")['href']
+            app_icon = result.find("img", class_="T75of bzqKMd")['src']
+        else:
+            app_name = result.find("span", class_="DdYX5").text
+            app_dev = result.find("span", class_="wMUdtb").text
+            app_rating = result.find("span", class_="w2kbF").text
+            _app_link = result.find("a", class_="Si6A0c Gy4nib")['href']
+            app_icon = result.find("img", class_="T75of stzEZd")['src']
+
         app_dev_link = (
             "https://play.google.com/store/apps/developer?id="
             + app_dev.replace(" ", "+")
         )
-        app_rating = (
-            result.find("div", class_="TT9eCd")["aria-label"]
-            .replace("Rated ", "⭐️ ")
-            .replace(" out of ", "/")
-            .replace(" stars", "", 1)
-            .replace(" stars", "⭐️")
-            .replace("five", "5")
-        )
-        app_link = "https://play.google.com" + result.find("a", class_="Qfxief")["href"]
-        app_icon = result.find("img", class_="T75of bzqKMd")["src"]
+        app_link = "https://play.google.com" + _app_link
 
         app_details = f"[📲]({app_icon}) **{app_name}**\n\n"
         app_details += f"`Developer :` [{app_dev}]({app_dev_link})\n"
-        app_details += f"`Rating :` {app_rating}\n"
+        app_details += f"`Rating :` {app_rating} ⭐️\n"
         app_details += f"`Features :` [View in Play Store]({app_link})"
         await message.edit(app_details, disable_web_page_preview=False)
     except IndexError:
