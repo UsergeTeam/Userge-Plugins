090738a0859d859d053f1345559e393196470b2a
None <52490534+rking32@users.noreply.github.com>
2022-11-03 19:41:49 +0530

    trying to fix cancel cmd on livesubs cmd

--- plugins/utils/livesubs/__main__.py
+++ plugins/utils/livesubs/__main__.py
@@ -31,18 +31,19 @@ async def live_subs(msg: Message):
             await msg.err("Chat Id Invalid")
     if chat:
         username = f"@{chat.username}" if chat.username else chat.title
-        while True:
-            if msg.process_is_canceled:
-                await msg.edit("`Live subs request Cancelled`")
-                break
-            try:
-                subs = await msg.client.get_chat_members_count(chat.id)
-                await msg.edit(
-                    f"**Live Members Count of {username}**\n\n"
-                    f"Members = `{subs}`"
-                )
-                await asyncio.sleep(3)
-            except FloodWait as fw:
-                await asyncio.sleep(fw.value)
+        with msg.cancel_callback():
+            while True:
+                if msg.process_is_canceled:
+                    await msg.edit("`Live subs request Cancelled`")
+                    break
+                try:
+                    subs = await msg.client.get_chat_members_count(chat.id)
+                    await msg.edit(
+                        f"**Live Members Count of {username}**\n\n"
+                        f"Members = `{subs}`"
+                    )
+                    await asyncio.sleep(3)
+                except FloodWait as fw:
+                    await asyncio.sleep(fw.value)
     else:
         await msg.err("chat id required")
