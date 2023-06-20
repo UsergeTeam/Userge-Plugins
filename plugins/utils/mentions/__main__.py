b136a4858e2b7a4eb943bd5eb6c80b8713c81983
Jigar varma <jigarverma2002@gmail.com>
2023-06-20 14:24:35 +0530

    Fix for invalid chat id while copying media from private chat [mentions] (#331)
    * Fix for invalid chat id while copying media from private chat [mentions]
    
    * Send media to log and attach link to alert msg
    
    * Fix Deepsource
    
    * Fix invalid link for private group
    
    * Typo

--- plugins/utils/mentions/__main__.py
+++ plugins/utils/mentions/__main__.py
@@ -8,7 +8,7 @@
 #
 # All rights reserved.
 
-from pyrogram.errors import PeerIdInvalid
+from pyrogram.errors import PeerIdInvalid, BadRequest
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram import enums
 
@@ -56,45 +56,72 @@ async def handle_mentions(msg: Message):
         return
 
     if msg.chat.type == enums.ChatType.PRIVATE:
+        is_private = True
         link = f"tg://openmessage?user_id={msg.chat.id}&message_id={msg.id}"
         text = f"{msg.from_user.mention} 💻 sent you a **Private message.**"
     else:
-        link = msg.link
+        is_private = False
+        if msg.chat.type == enums.ChatType.GROUP:
+            link = f"tg://openmessage?chat_id={str(msg.chat.id).strip('-')}&message_id={msg.id}"
+        else:
+            link = msg.link
         text = f"{msg.from_user.mention} 💻 tagged you in **{msg.chat.title}.**"
     text += f"\n\n[Message]({link}):" if not userge.has_bot else "\n\n**Message:**"
     if msg.text:
         text += f"\n`{msg.text}`"
-
-    button = InlineKeyboardButton(text="📃 Message Link", url=link)
+    buttons = [[InlineKeyboardButton(text="📃 Message Link", url=link)]]
 
     client = userge.bot if userge.has_bot else userge
+    if not msg.text:
+        if is_private:
+            chat_id = config.LOG_CHANNEL_ID
+            media_client = userge
+        else:
+            media_client = client
+            chat_id = userge.id if userge.has_bot else config.LOG_CHANNEL_ID
     try:
+        if not msg.text:
+            try:
+                sentMedia = await media_client.copy_message(
+                    chat_id,
+                    msg.chat.id,
+                    msg.id
+                )
+            except (PeerIdInvalid, BadRequest):
+                sentMedia = await userge.copy_message(
+                    config.LOG_CHANNEL_ID,
+                    msg.chat.id,
+                    msg.id
+                )
+            buttons.append([InlineKeyboardButton(text="📃 Media Link", url=sentMedia.link)])
         await client.send_message(
             chat_id=userge.id if userge.has_bot else config.LOG_CHANNEL_ID,
             text=text,
             disable_web_page_preview=True,
-            reply_markup=InlineKeyboardMarkup([[button]])
+            reply_markup=InlineKeyboardMarkup(buttons)
         )
-        if not msg.text:
-            await client.copy_message(
-                userge.id if userge.has_bot else config.LOG_CHANNEL_ID,
-                msg.chat.id,
-                msg.id
-            )
     except PeerIdInvalid:
         if userge.dual_mode:
-            await userge.send_message(userge.id, "/start")
+            await userge.send_message(userge.bot.id, "/start")
+            if not msg.text:
+                try:
+                    sentMedia = await media_client.copy_message(
+                        chat_id,
+                        msg.chat.id,
+                        msg.id
+                    )
+                except (PeerIdInvalid, BadRequest):
+                    sentMedia = await userge.copy_message(
+                        config.LOG_CHANNEL_ID,
+                        msg.chat.id,
+                        msg.id
+                    )
+                buttons.append([InlineKeyboardButton(text="📃 Media Link", url=sentMedia.link)])
             await client.send_message(
                 chat_id=userge.id if userge.has_bot else config.LOG_CHANNEL_ID,
                 text=text,
                 disable_web_page_preview=True,
-                reply_markup=InlineKeyboardMarkup([[button]])
+                reply_markup=InlineKeyboardMarkup(buttons)
             )
-            if not msg.text:
-                await client.copy_message(
-                    userge.id if userge.has_bot else config.LOG_CHANNEL_ID,
-                    msg.chat.id,
-                    msg.id
-                )
         else:
             raise
